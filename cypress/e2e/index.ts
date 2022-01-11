eea269f5b246a4986cb5a7ae6b3493a63a152300
harika.kotipalli <harika.1818@gmail.com>
2022-01-11 16:43:33 -0500

    DEVHUB-949 Adding cypress testing library

--- cypress/e2e/index.ts
+++ cypress/e2e/index.ts
@@ -1,6 +1,6 @@
 describe('index page', () => {
     it('should have a descriptive header', () => {
-        cy.visit('http://localhost:3000')
+        cy.visit('/')
             .get('h1')
             .should('have.text', 'MongoDB Developer Center')
     });
