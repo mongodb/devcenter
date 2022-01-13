b28d262d389e1653e1272dcaefb3022cdb54ac9e
Matt Colella <matt.colella@mongodb.com>
2022-01-13 15:56:06 -0500

    Add failing test to see if workflow artifacts are generated.

--- cypress/e2e/index.ts
+++ cypress/e2e/index.ts
@@ -1,5 +1,5 @@
 describe('index page', () => {
     it('should have a descriptive header', () => {
-        cy.visit('/').get('h1').should('have.text', 'MongoDB Developer Center');
+        cy.visit('/').get('h1').should('have.text', 'TEST');
     });
 });
