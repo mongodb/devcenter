7f3b48dc903b3270a8587451389bd5eea023a4cf
Matt Colella <matt.colella@mongodb.com>
2022-01-14 15:59:53 -0500

    Add grid documentation.

--- src/components/grid.tsx
+++ src/components/grid.tsx
@@ -1,6 +1,16 @@
 import StyledGrid, { StyledGridColumn } from '../styled/grid';
-import { GridProps } from '../interfaces/components/grid';
-
+import { GridColumnProps, GridProps } from '../interfaces/components/grid';
+/**
+ * Component used to create a grid for its child components.
+ * @param props.as (optional) -  The type of tag that the grid element will be.
+ * @param props.children - Child components of this component (can be passed inside this component).
+ * @param props.columnGutter (optional) -  Overrides the default value of 16px for space between columns.
+ * @param props.rowGutter (optional) -  Overrides the default value of 0px for space between rows.
+ * @param props.verticalAlign (optional) -  Pass this 'align-items' attribute to the grid element.
+ * @param props.horizontalAlign (optional) -  Passes this as 'justify-items' attribute to the grid element.
+ * @param props.className (optional) -  Passes this as 'className' to the grid element.
+ * @param props.id (optional) - Passes this as 'id' to the grid element.
+ */
 const Grid = ({
     as,
     children,
@@ -24,5 +34,30 @@ const Grid = ({
     </StyledGrid>
 );
 
+/**
+ * Component used to create a grid column to be used as a child of the Grid component.
+ * @param props.as (optional) -  The type of tag that the grid element will be.
+ * @param props.children - Child components of this component (can be passed inside this component).
+ * @param props.phoneColumns (optional) - Override default columns (4) that this component spans for phone screens.
+ * @param props.tabletColumns (optional) - Override default columns (8) that this component spans for tablet screens.
+ * @param props.desktopColumns (optional) - Override default columns (12) that this component spans for desktop screens.
+ */
+const GridColumn = ({
+    as,
+    children,
+    phoneColumns,
+    tabletColumns,
+    desktopColumns,
+}: GridColumnProps) => (
+    <StyledGridColumn
+        as={as}
+        phoneColumns={phoneColumns}
+        tabletColumns={tabletColumns}
+        desktopColumns={desktopColumns}
+    >
+        {children}
+    </StyledGridColumn>
+);
+
 export default Grid;
-export { StyledGridColumn as GridColumm }; // All the functionality is in the styling, so just pass this on.
+export { GridColumn };
--- src/pages/topics/[slug].tsx
+++ src/pages/topics/[slug].tsx
@@ -1,16 +1,46 @@
 import type { NextPage } from 'next';
 import type { GetStaticProps, GetStaticPaths } from 'next';
 import { ParsedUrlQuery } from 'querystring';
+import styled from '@emotion/styled'; // TEMPORARY
 
+import Grid, { GridColumn } from '../../components/grid';
+
+////////////////////////////////////////////////////////////////////////
+//////////////////////////////TEMPORARY/////////////////////////////////
+interface placeholder {
+    color?: string;
+    height?: string;
+    width?: string;
+}
+
+const Placeholder = styled.div<placeholder>`
+    background-color: ${props => (props.color ? props.color : '#ccc')};
+    max-width: 100%;
+    overflow: hidden;
+    height: ${props => (props.height ? props.height : '100')}px;
+    width: ${props => (props.width ? props.width : '1400')}px;
+`;
+
+////////////////////////////////////////////////////////////////////////
 interface HomeProps {
     slug: string;
 }
 
 const Topic: NextPage<HomeProps> = ({ slug }) => (
-    <>
-        <h1>MongoDB Developer Center</h1>
-        <h2>{slug}</h2>
-    </>
+    <Grid>
+        <GridColumn desktopColumns={5}>
+            <Placeholder color="red">{slug}</Placeholder>
+        </GridColumn>
+        <GridColumn desktopColumns={7}>
+            <Placeholder color="blue">{slug}</Placeholder>
+        </GridColumn>
+        <GridColumn desktopColumns={7}>
+            <Placeholder color="red">{slug}</Placeholder>
+        </GridColumn>
+        <GridColumn desktopColumns={5}>
+            <Placeholder color="blue">{slug}</Placeholder>
+        </GridColumn>
+    </Grid>
 );
 
 export default Topic;
