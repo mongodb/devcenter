19429dee167f3834a71bc67ac09cdd334c9a9425
Matt Colella <matt.colella@mongodb.com>
2022-01-24 17:06:57 -0500

    implement apollo with countries endpoint

---
+++ src/interfaces/country.ts
@@ -0,0 +1,5 @@
+export interface Country {
+    code: string;
+    name: string;
+    emoji: string;
+}
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,23 +1,30 @@
 import type { NextPage } from 'next';
 
-// import { fetchApiRest } from '../requests/strapi-api';
-import { ArticleResponse } from '../interfaces/responses/article-response';
+import { getCountries } from '../requests/graphql/example';
+import { Country } from '../interfaces/country';
 
 interface HomeProps {
-    allArticles: ArticleResponse[];
+    countries: Country[];
 }
 
-const Home: NextPage<HomeProps> = () => (
+const Home: NextPage<HomeProps> = ({ countries }) => (
     <>
         <h1>MongoDB Developer Center</h1>
+        <ul>
+            {countries.map(c => (
+                <li key={c.code}>
+                    {c.name} | {c.code} | {c.emoji}
+                </li>
+            ))}
+        </ul>
     </>
 );
 
 export default Home;
 
-// export async function getServerSideProps({}) {
-//     const allArticles = (await fetchApiRest('articles')) || [];
-//     return {
-//         props: { allArticles },
-//     };
-// }
+export async function getServerSideProps({}) {
+    const countries = await getCountries();
+    return {
+        props: { countries },
+    };
+}
