20148190f2b84fdb294c7ea5fda45f2e017aa61e
Greg Campoverde <greg.c@mongodb.com>
2022-02-14 22:04:57 -0500

    Added state for dropdowns

--- src/components/subnavigation/navbar.tsx
+++ src/components/subnavigation/navbar.tsx
@@ -5,35 +5,52 @@ import React, { useRef, useState } from 'react';
 import { SecondaryNav } from '../../styled/nav-bar';
 import styled from '@emotion/styled';
 
-const NavBar: React.FunctionComponent = () => {
-    const [isOpen, setIsOpen] = useState(false);
+const DropDownWrapper = styled.ul`
+    position: absolute;
+    top: 28px;
+    left: 0;
+    padding: 15px;
+    background-color: #ccc;
+    min-width: 300px;
+`;
 
+const ListItem = styled.li`
+    padding: 0.5rem 0;
+    list-style: none;
+
+    > a {
+        color: #fff;
+        text-decoration: none;
+    }
+`;
+
+const DropDown = ({ items, ...props }: any) =>
+    items.map(({ text, path }: any) => (
+        <ListItem key={text}>
+            <Link href={path} {...props}>
+                {text}
+            </Link>
+        </ListItem>
+    ));
+
+const CallToActionDropDown = ({ text, dropdownItems }: any) => {
+    const [isOpen, setIsOpen] = useState(false);
     const onClickShowMenu = () => {
         setIsOpen(!isOpen);
     };
+    return (
+        <>
+            <button onClick={onClickShowMenu}>{text}</button>
+            {isOpen && (
+                <DropDownWrapper>
+                    <DropDown items={dropdownItems} />
+                </DropDownWrapper>
+            )}
+        </>
+    );
+};
 
-    const DropDownWrapper = styled.ul`
-        position: absolute;
-        top: 28px;
-        left: 0;
-        padding: 15px;
-        background-color: #ccc;
-        min-width: 300px;
-    `;
-
-    const ListItem = styled.li`
-        padding: 0.5rem 0;
-        list-style: none;
-
-        > a {
-            color: #fff;
-            text-decoration: none;
-        }
-    `;
-
-    const DropDown = ({ items, ...props }: any) =>
-        items.map(({ text, path }: any) => <ListItem key={text}><Link href={path} {...props}>{text}</Link></ListItem>);
-
+const NavBar: React.FunctionComponent = () => {
     return (
         <>
             <SecondaryNav>
@@ -41,12 +58,10 @@ const NavBar: React.FunctionComponent = () => {
                 {navButtons.map(({ text, path, dropdown, dropdownItems }) => (
                     <SecondaryNavLink key={text}>
                         {dropdown ? (
-                            <>
-                                <button onClick={onClickShowMenu}>
-                                    {text}
-                                </button>
-                                {isOpen && <DropDownWrapper><DropDown items={dropdownItems} /></DropDownWrapper>}
-                            </>
+                            <CallToActionDropDown
+                                text={text}
+                                dropdownItems={dropdownItems}
+                            />
                         ) : (
                             <Link href={path}>{text}</Link>
                         )}
