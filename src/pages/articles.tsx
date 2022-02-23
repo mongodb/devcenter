a24107bdb2c0f3fc18ff052f69086317b68d1dd8
harika.kotipalli <harika.1818@gmail.com>
2022-02-23 18:45:31 -0500

    DEVHUB-1000 fixing light house issue

--- src/pages/articles.tsx
+++ src/pages/articles.tsx
@@ -1,36 +1,9 @@
 import React from 'react';
 import { NextPage } from 'next';
-import { CardContent } from '../interfaces/card-content';
-import { PillCategory } from '../types/pill-category';
-import getL1Content from '../requests/get-l1-content';
-import { CONTENT_CATEGORIES } from '../constants/content-categories';
-import FeatureCard from '../components/cards/featurecard/featured-card';
 
 const Articles: NextPage = () => (
     <>
-        <div>FEATURED JUMBO CARDS</div>
-        <div>
-            {CONTENT_CATEGORIES.map((category: PillCategory) =>
-                getL1Content()
-                    .filter(
-                        (result: CardContent) =>
-                            result.pillCategory === category
-                    )
-                    .map((result: CardContent) => (
-                        <div key={result.title}>
-                            <FeatureCard
-                                key={result.title}
-                                pillCategory={result.pillCategory}
-                                thumbnail={result.thumbnail}
-                                title={result.title}
-                                description={result.description}
-                                tags={result.tags}
-                                contentDate={result.contentDate}
-                            />
-                        </div>
-                    ))
-            )}
-        </div>
+        <h1>Articles</h1>
     </>
 );
 
--- src/pages/tutorials.tsx
+++ src/pages/tutorials.tsx
@@ -5,6 +5,7 @@ import getL1Content from '../requests/get-l1-content';
 import { CardContent } from '../interfaces/card-content';
 import SharedCard from '../components/cards/sharedcard/shared-card';
 import React from 'react';
+import FeatureCard from '../components/cards/featurecard/featured-card';
 
 const Tutorials: NextPage = () => (
     <>
@@ -32,6 +33,29 @@ const Tutorials: NextPage = () => (
                             description={result.description}
                             contentDate={result.contentDate}
                         />
+                    ))
+            )}
+        </div>
+        <div>FEATURED JUMBO CARDS</div>
+        <div>
+            {CONTENT_CATEGORIES.map((category: PillCategory) =>
+                getL1Content()
+                    .filter(
+                        (result: CardContent) =>
+                            result.pillCategory === category
+                    )
+                    .map((result: CardContent) => (
+                        <div key={result.title}>
+                            <FeatureCard
+                                key={result.title}
+                                pillCategory={result.pillCategory}
+                                thumbnail={result.thumbnail}
+                                title={result.title}
+                                description={result.description}
+                                tags={result.tags}
+                                contentDate={result.contentDate}
+                            />
+                        </div>
                     ))
             )}
         </div>
