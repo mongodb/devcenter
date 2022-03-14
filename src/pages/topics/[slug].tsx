1832ddac23225cce003235ff6180fde0f0b0b919
Greg Campoverde <greg.c@mongodb.com>
2022-03-14 17:43:22 -0400

    Removed unneeded export

--- src/pages/topics/[slug].tsx
+++ src/pages/topics/[slug].tsx
@@ -34,7 +34,7 @@ interface TopicProps {
     tertiaryNavItems: TertiaryNavItem[];
 }
 
-export const sideNavStyles = (rowCount: number) => ({
+const sideNavStyles = (rowCount: number) => ({
     display: ['none', null, null, null, 'block'],
     gridColumn: ['span 6', null, 'span 8', 'span 12', 'span 3'],
     nav: {
