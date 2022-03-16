82f54ea69978fcd55ff45d0cb716200d4c9ec46d
harika.kotipalli <harika.1818@gmail.com>
2022-03-16 12:33:22 -0400

    devhub-1025 Adding dynamic folder structure to support dynamic l1 and l2 url structuring

---
+++ src/data/l2-products.ts
@@ -0,0 +1,34 @@
+import { Taxonomy } from '../interfaces/taxonomy';
+
+export const l2Products: Taxonomy[] = [
+    {
+        name: 'Change Streams',
+        slug: 'atlas/change-streams',
+        description:
+            'Blurb consisting of a description of the title or tag for the page. No more than 2 - 3 lines, and 5 column max',
+        ctas: [
+            { text: 'Primary CTA', url: 'https://www.mongodb.com/atlas' },
+            {
+                text: 'Secondary CTA',
+                url: 'https://www.mongodb.com/cloud/atlas/register',
+            },
+        ],
+        topics: [],
+        relatedTopics: [],
+    },
+    {
+        name: 'Document Model',
+        slug: 'atlas/document-model',
+        description:
+            'Blurb consisting of a description of the title or tag for the page. No more than 2 - 3 lines, and 5 column max',
+        ctas: [
+            { text: 'Primary CTA', url: 'https://www.mongodb.com/atlas' },
+            {
+                text: 'Secondary CTA',
+                url: 'https://www.mongodb.com/cloud/atlas/register',
+            },
+        ],
+        topics: [],
+        relatedTopics: [],
+    },
+];
---
+++ src/data/taxonomy-collection-types.ts
@@ -0,0 +1,12 @@
+type Props = {
+    [key: string]: string;
+};
+
+export const taxonomyToCategoryMapping: Props = {
+    l1Products: 'product',
+    l2Products: 'product',
+    languages: 'language',
+    stacks: 'stack',
+    technologies: 'technology',
+    frameworks: 'framework',
+};
