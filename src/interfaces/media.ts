8d3e9c51df8de71639b1abb79fb0b324746ea868
harika.kotipalli <harika.1818@gmail.com>
2022-05-04 15:56:26 -0400

    devhub-1044 replacing images in podcasts and video pages with podcast player and youtube player

--- src/interfaces/media.ts
+++ src/interfaces/media.ts
@@ -1,11 +1,3 @@
-import {
-    L1ProductTag,
-    ProgrammingLanguageTag,
-    TechnologyTag,
-} from './tag-type-response';
-import { OtherTags } from './other-tags';
-import { PrimaryTags } from './primary-tags';
-
 export interface Media {
     description?: string;
     publishDate: string;
