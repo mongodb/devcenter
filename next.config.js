adaa5bbaa391970cd281c4793e5912a7d4f5f5bb
Amanda J. Henri <ajhenri@mongodb.com>
2022-05-03 23:29:48 -0400

    Add healthz endpoint

--- next.config.js
+++ next.config.js
@@ -3,6 +3,14 @@ const configVals = {
     images: {
         domains: ['mongodb-devhub-cms.s3.us-west-1.amazonaws.com'],
     },
+    rewrites: async () => {
+        return [
+            {
+                source: '/healthz',
+                destination: '/api/health',
+            },
+        ];
+    },
 };
 if (process.env.ANALYZE === 'true') {
     const withBundleAnalyzer = require('@next/bundle-analyzer')({
