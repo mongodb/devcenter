5da2bd5e4943f32551d954a2584ef6f8bee7db4e
harika.kotipalli <harika.1818@gmail.com>
2022-05-05 11:58:13 -0400

    Adding authors api call, replacing mockup authors with real authors

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -3,7 +3,6 @@ import { GetStaticProps } from 'next';
 import { clientFactory } from '../utils/client-factory';
 import { getArticles } from '../api-requests/get-articles';
 import { Article } from '../interfaces/article';
-import { getAllAuthors } from '../service/get-all-authors';
 
 interface HomeProps {
     articles: Article[];
@@ -29,7 +28,6 @@ export default Home;
 export const getStaticProps: GetStaticProps = async ({}) => {
     const client = clientFactory('ApolloREST', process.env.STRAPI_URL);
     const articles = await getArticles(client);
-    const authors = await getAllAuthors();
     return {
         props: { articles },
     };
