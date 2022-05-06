fa052d69214b2110f401e7f5c3da1a6e71e5c711
harika.kotipalli <harika.1818@gmail.com>
2022-05-06 10:37:53 -0400

    devhub-1066 temporarily commenting adding series logic

--- src/service/get-all-content.ts
+++ src/service/get-all-content.ts
@@ -60,7 +60,7 @@ export const mapPodcastsToContentItems = (
             item.image = { url: p.thumbnailUrl, alt: 'randomAlt' };
         }
         item.podcastFileUrl = p.casted_slug;
-        addSeriesToItem(item, 'podcast', podcastSeries);
+        //addSeriesToItem(item, 'podcast', podcastSeries);
         items.push(item);
     });
     return items;
@@ -91,7 +91,7 @@ export const mapVideosToContentItems = (
         };
 
         item.videoId = v.videoId;
-        addSeriesToItem(item, 'video', videoSeries);
+        //addSeriesToItem(item, 'video', videoSeries);
         items.push(item);
     });
     return items;
@@ -132,7 +132,7 @@ export const mapArticlesToContentItems = (
         if (a.image) {
             item.image = { url: a.image.url, alt: a.image.alt || 'random alt' };
         }
-        addSeriesToItem(item, 'article', articleSeries);
+        //addSeriesToItem(item, 'article', articleSeries);
         items.push(item);
     });
     return items;
