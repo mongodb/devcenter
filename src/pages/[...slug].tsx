5a601ebefa42b2fa0098fd04a1963b4a8a760b4b
harika.kotipalli <harika.1818@gmail.com>
2022-05-09 14:01:51 -0400

    removing unwanted comments

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -172,10 +172,6 @@ const ContentPage: NextPage<ContentPageProps> = ({
     const [requestContentModalStage, setRequestContentModalStage] =
         useState<requestContentModalStages>('closed');
 
-    const site = 'https://mongodb.com/developer';
-    const canonicalURL = site + '/' + slug;
-    console.log(canonicalURL);
-
     // const relatedContent = getRelatedContent(slug);
     // const slugList = slug.split('/');
 
