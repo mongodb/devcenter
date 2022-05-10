41c2b3ec44729f20dbbee97717e90255db67b6b8
Matt Colella <matt.colella@mongodb.com>
2022-05-10 12:07:19 -0400

    Uncomment podcast and video logic

--- src/service/get-all-videos.ts
+++ src/service/get-all-videos.ts
@@ -3,35 +3,34 @@ import { Video } from '../interfaces/video';
 import getAllVideosFromAPI from '../api-requests/get-all-videos';
 
 export const getAllVideos = async (): Promise<Video[]> => {
-    // const videos = await getAllVideosFromAPI(STRAPI_CLIENT);
-    // videos.forEach(v => {
-    //     if (v.otherTags) {
-    //         v.otherTags.contentType = {
-    //             contentType: 'Video',
-    //             calculatedSlug: '/videos',
-    //         };
-    //     } else {
-    //         v.otherTags = {
-    //             contentType: {
-    //                 contentType: 'Video',
-    //                 calculatedSlug: '/videos',
-    //             },
-    //         };
-    //     }
-    //     if (v.l1Product) {
-    //         v.otherTags.l1Product = v.l1Product;
-    //     }
-    //     if (v.l2Product) {
-    //         v.otherTags.l2Product = v.l2Product;
-    //     }
-    //     if (v.programmingLanguage) {
-    //         v.otherTags.programmingLanguage = v.programmingLanguage;
-    //     }
-    //     if (v.technology) {
-    //         v.otherTags.technology = v.technology;
-    //     }
-    // });
+    const videos = await getAllVideosFromAPI(STRAPI_CLIENT);
+    videos.forEach(v => {
+        if (v.otherTags) {
+            v.otherTags.contentType = {
+                contentType: 'Video',
+                calculatedSlug: '/videos',
+            };
+        } else {
+            v.otherTags = {
+                contentType: {
+                    contentType: 'Video',
+                    calculatedSlug: '/videos',
+                },
+            };
+        }
+        if (v.l1Product) {
+            v.otherTags.l1Product = v.l1Product;
+        }
+        if (v.l2Product) {
+            v.otherTags.l2Product = v.l2Product;
+        }
+        if (v.programmingLanguage) {
+            v.otherTags.programmingLanguage = v.programmingLanguage;
+        }
+        if (v.technology) {
+            v.otherTags.technology = v.technology;
+        }
+    });
 
-    // return videos;
-    return [];
+    return videos;
 };
