eb1494abf6415ab4b0b97bba54c874adc37cff25
Matt Colella <matt.colella@mongodb.com>
2022-05-12 17:32:37 -0400

    Add code level tag to cards

--- src/components/icons/BaseIcon.tsx
+++ src/components/icons/BaseIcon.tsx
@@ -1,15 +1,17 @@
 export interface BaseIconProps {
     className?: string;
+    isFilled?: boolean;
 }
 
 const BaseIcon: React.FunctionComponent<BaseIconProps> = ({
     className,
     children,
+    isFilled = false,
 }) => {
     return (
         <svg
             viewBox="0 0 24 24"
-            fill="none"
+            {...(isFilled ? {} : { fill: 'none' })}
             xmlns="http://www.w3.org/2000/svg"
             sx={{
                 width: 'inc20',
