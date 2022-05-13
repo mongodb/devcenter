47d82b1383757d688d12981d61756c914a7bad39
Amanda J. Henri <ajhenri@mongodb.com>
2022-05-13 16:15:54 -0400

    Add canonical link tag for all pages

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,24 @@
 import type { AppProps } from 'next/app';
 import Head from 'next/head';
+import getConfig from 'next/config';
+import { useRouter } from 'next/router';
 import theme from '@mdb/flora/theme';
 import { ThemeProvider } from '@theme-ui/core';
 
 import Layout from '../components/layout';
 
 function MyApp({ Component, pageProps }: AppProps) {
+    const router = useRouter();
+    const { publicRuntimeConfig } = getConfig();
+    const canonicalUrl = publicRuntimeConfig.absoluteBasePath + router.asPath;
+
     return (
         <>
             <Head>
                 <title>MongoDB Developer Center</title>
                 <meta name="description" content="MongoDB Developer Center" />
                 <link rel="icon" href="/developer/favicon.ico" />
+                <link rel="canonical" href={`${canonicalUrl}`} />
             </Head>
             <ThemeProvider theme={theme}>
                 <Layout>
