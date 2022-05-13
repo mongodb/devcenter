7220c56c45505d1d3755df5cd5566c8fc3fdb27d
Amanda J. Henri <ajhenri@mongodb.com>
2022-05-13 16:15:10 -0400

    Add SITE_URL and allow for access in client code

--- next.config.js
+++ next.config.js
@@ -1,5 +1,10 @@
 const { redirects } = require('./config/redirects');
 
+const siteURL = process.env.VERCEL_URL
+    ? process.env.VERCEL_URL
+    : process.env.SITE_URL;
+const httpProtocol = siteURL == 'localhost:3000' ? 'http' : 'https';
+
 const configVals = {
     basePath: '/developer',
     reactStrictMode: true,
@@ -7,6 +12,9 @@ const configVals = {
         domains: ['mongodb-devhub-cms.s3.us-west-1.amazonaws.com'],
     },
     redirects: redirects,
+    publicRuntimeConfig: {
+        absoluteBasePath: `${httpProtocol}://${siteURL}/developer`,
+    },
 };
 if (process.env.ANALYZE === 'true') {
     const withBundleAnalyzer = require('@next/bundle-analyzer')({
