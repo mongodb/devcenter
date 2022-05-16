6302ac25866365be6df4fc977eab3382bd5ad6fe
Greg Campoverde <greg.c@mongodb.com>
2022-05-16 15:59:18 -0400

    Added styles for topics

--- src/pages/topics.tsx
+++ src/pages/topics.tsx
@@ -2,6 +2,9 @@ import { GetStaticProps } from 'next';
 import { clientFactory } from '../utils/client-factory';
 import { getAllMetaInfo } from '../service/get-all-meta-info';
 import { getURLPath } from '../utils/format-url-path';
+import Hero from '../components/hero';
+import { Crumb } from '../components/breadcrumbs/types';
+import { GridLayout, TypographyScale } from '@mdb/flora';
 
 interface Cta {
     text: string;
@@ -35,11 +38,29 @@ const getProductsByName = (productsArray: string[], topics: TopicsProps[]) => {
 const ULStyles = {
     listStyleType: 'none',
     padding: 0,
-    margin: 0,
-    columnCount: 6,
+    marginBottom: ['section20', 'section20', 'section20', 'section40'],
+    display: 'grid',
+    gridTemplateColumns: [
+        'repeat(2, 1fr)',
+        'repeat(2, 1fr)',
+        'repeat(4, 1fr)',
+        'repeat(4, 1fr)',
+        'repeat(6, 1fr)',
+    ],
+    li: {
+        // paddingBottom: ['inc30', 'inc30', 'inc30', 'inc40'],
+        'a:hover': {
+            textDecoration: 'underline !important',
+        },
+    },
 };
 
 const Topic = ({ topics }: { topics: TopicsProps[] }) => {
+    const crumbs: Crumb[] = [
+        { text: 'MongoDB Developer Center', url: '/developer' },
+        { text: 'Developer Topics', url: '/developer/topics' },
+    ];
+
     const getExpertiseLevels = topics.filter(
         ({ category }) => category === 'ExpertiseLevel'
     );
@@ -64,48 +85,131 @@ const Topic = ({ topics }: { topics: TopicsProps[] }) => {
 
     return (
         <>
-            <h1>Topics</h1>
-            <h2>Expertise Level</h2>
-            <ul sx={ULStyles}>
-                {getExpertiseLevels?.map(({ tagName, slug }) => (
-                    <li key={tagName}>
-                        <a href={getURLPath(slug)}>{tagName}</a>
-                    </li>
-                ))}
-            </ul>
-            <h2>Languages</h2>
-            <ul sx={ULStyles}>
-                {getLanguages?.map(({ tagName, slug }) => (
-                    <li key={tagName}>
-                        <a href={getURLPath(slug)}>{tagName}</a>
-                    </li>
-                ))}
-            </ul>
-            <h2>Technologies</h2>
-            <ul sx={ULStyles}>
-                {getTechnologies?.map(({ tagName, slug }) => (
-                    <li key={tagName}>
-                        <a href={getURLPath(slug)}>{tagName}</a>
-                    </li>
-                ))}
-            </ul>
-            <h2>Products</h2>
-            {[Atlas, Compass, Connectors, MongoDB, OpsManager, Realm].map(
-                ({ slug, tagName, topics }) => (
-                    <div key={tagName}>
-                        <h3>
-                            <a href={getURLPath(slug)}>{tagName}</a>
-                        </h3>
-                        <ul sx={ULStyles}>
-                            {topics?.map(({ title, href }) => (
-                                <li key={title}>
-                                    <a href={getURLPath(href)}>{title}</a>
-                                </li>
-                            ))}
-                        </ul>
-                    </div>
-                )
-            )}
+            <Hero crumbs={crumbs} name="All Topics" />
+            <GridLayout
+                sx={{
+                    paddingLeft: [
+                        'inc40',
+                        'inc40',
+                        'inc50',
+                        'inc70',
+                        'inc70',
+                        0,
+                    ],
+                    paddingRight: [
+                        'inc40',
+                        'inc40',
+                        'inc50',
+                        'inc70',
+                        'inc70',
+                        0,
+                    ],
+                }}
+            >
+                <div
+                    sx={{
+                        gridColumn: ['span 6', 'span 8', 'span 12', 'span 12'],
+                        marginTop: ['inc40', 'inc40', 'inc40', 'inc50'],
+                    }}
+                >
+                    <TypographyScale
+                        sx={{
+                            marginBottom: ['inc30', 'inc30', 'inc30', 'inc50'],
+                        }}
+                        variant="heading5"
+                    >
+                        Expertise Level
+                    </TypographyScale>
+                    <ul sx={ULStyles}>
+                        {getExpertiseLevels?.map(({ tagName, slug }) => (
+                            <li key={tagName}>
+                                <TypographyScale variant="body1">
+                                    <a href={getURLPath(slug)}>{tagName}</a>
+                                </TypographyScale>
+                            </li>
+                        ))}
+                    </ul>
+                    <TypographyScale
+                        sx={{
+                            marginBottom: ['inc30', 'inc30', 'inc30', 'inc50'],
+                        }}
+                        variant="heading5"
+                    >
+                        Languages
+                    </TypographyScale>
+                    <ul sx={ULStyles}>
+                        {getLanguages?.map(({ tagName, slug }) => (
+                            <li key={tagName}>
+                                <TypographyScale variant="body1">
+                                    <a href={getURLPath(slug)}>{tagName}</a>
+                                </TypographyScale>
+                            </li>
+                        ))}
+                    </ul>
+                    <TypographyScale
+                        sx={{
+                            marginBottom: ['inc30', 'inc30', 'inc30', 'inc50'],
+                        }}
+                        variant="heading5"
+                    >
+                        Technologies
+                    </TypographyScale>
+                    <ul sx={ULStyles}>
+                        {getTechnologies?.map(({ tagName, slug }) => (
+                            <li key={tagName}>
+                                <TypographyScale variant="body1">
+                                    <a href={getURLPath(slug)}>{tagName}</a>
+                                </TypographyScale>
+                            </li>
+                        ))}
+                    </ul>
+                    <TypographyScale
+                        sx={{ marginBottom: 0 }}
+                        variant="heading5"
+                    >
+                        Products
+                    </TypographyScale>
+                    {[
+                        Atlas,
+                        Compass,
+                        Connectors,
+                        MongoDB,
+                        OpsManager,
+                        Realm,
+                    ].map(({ slug, tagName, topics }) => (
+                        <div key={tagName}>
+                            <TypographyScale
+                                sx={{
+                                    fontWeight: '700',
+                                    marginTop: 'inc50',
+                                    marginBottom: [
+                                        'inc30',
+                                        'inc30',
+                                        'inc30',
+                                        'inc30',
+                                    ],
+                                }}
+                                variant="heading6"
+                            >
+                                <a href={getURLPath(slug)}>{tagName}</a>
+                            </TypographyScale>
+                            {!!topics.length && (
+                                <ul sx={{ ...ULStyles, marginBottom: 'inc90' }}>
+                                    {topics?.map(({ title, href }) => (
+                                        <li key={title}>
+                                            <TypographyScale variant="body1">
+                                                <a href={getURLPath(href)}>
+                                                    {title}
+                                                </a>
+                                            </TypographyScale>
+                                        </li>
+                                    ))}
+                                </ul>
+                            )}
+                        </div>
+                    ))}
+                </div>
+            </GridLayout>
         </>
     );
 };
