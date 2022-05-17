b014695631747f2a98621469e8b7aef1f33eab12
Greg Campoverde <greg.c@mongodb.com>
2022-05-16 20:31:47 -0400

    Added back original state of files

--- src/service/get-all-videos.ts
+++ src/service/get-all-videos.ts
@@ -4,33 +4,33 @@ import getAllVideosFromAPI from '../api-requests/get-all-videos';
 
 export const getAllVideos = async (): Promise<Video[]> => {
     const videos = await getAllVideosFromAPI(STRAPI_CLIENT);
-    // videos.forEach(v => {
-    //     if (v.otherTags) {
-    //         v.otherTags.contentType = {
-    //             contentType: 'Video',
-    //             calculatedSlug: '/videos',
-    //         };
-    //     } else {
-    //         v.otherTags = {
-    //             contentType: {
-    //                 contentType: 'Video',
-    //                 calculatedSlug: '/videos',
-    //             },
-    //         };
-    //     }
-    //     if (v.l1Product) {
-    //         v.otherTags.l1Product = v.l1Product;
-    //     }
-    //     if (v.l2Product) {
-    //         v.otherTags.l2Product = v.l2Product;
-    //     }
-    //     if (v.programmingLanguage) {
-    //         v.otherTags.programmingLanguage = v.programmingLanguage;
-    //     }
-    //     if (v.technology) {
-    //         v.otherTags.technology = v.technology;
-    //     }
-    // });
+    videos.forEach(v => {
+        if (v.otherTags) {
+            v.otherTags.contentType = {
+                contentType: 'Video',
+                calculatedSlug: '/videos',
+            };
+        } else {
+            v.otherTags = {
+                contentType: {
+                    contentType: 'Video',
+                    calculatedSlug: '/videos',
+                },
+            };
+        }
+        if (v.l1Product) {
+            v.otherTags.l1Product = v.l1Product;
+        }
+        if (v.l2Product) {
+            v.otherTags.l2Product = v.l2Product;
+        }
+        if (v.programmingLanguage) {
+            v.otherTags.programmingLanguage = v.programmingLanguage;
+        }
+        if (v.technology) {
+            v.otherTags.technology = v.technology;
+        }
+    });
 
     return videos;
 };
