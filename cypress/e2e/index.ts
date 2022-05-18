d9a4a09dfda1acaeca838d5a6eb0b93af5f34f24
Matt Colella <matt.colella@mongodb.com>
2022-05-18 18:12:22 -0400

    fix test

--- cypress/e2e/index.ts
+++ cypress/e2e/index.ts
@@ -1,6 +1,6 @@
 describe('index page', () => {
     beforeEach(() => {
-        cy.visit('/developer');
+        cy.visit('/');
     });
     it('should have a descriptive header.', () => {
         cy.get('h1').should('have.text', 'MongoDB Developer Center');
