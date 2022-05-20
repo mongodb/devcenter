c9aa7907036b235485fb3db39bd9b476535acece
Amanda J. Henri <ajhenri@mongodb.com>
2022-05-20 19:07:55 -0400

    Remove mistaken use of origin. Replace search string in nextUrl instead

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,16 +2,24 @@ import { NextRequest, NextResponse } from 'next/server';
 import { rewrites } from '../../config/rewrites';
 
 export async function middleware(req: NextRequest) {
-    const { origin, pathname } = req.nextUrl;
+    const { pathname } = req.nextUrl;
 
     // Handles consistent navigation search as well as
     // redirect for /learn page.
     if (pathname == '/learn') {
         const s = req.nextUrl.searchParams.get('text');
         if (s && s.length > 0) {
-            return NextResponse.redirect(`${origin}/developer/search?s=${s}`);
+            const { href, search } = req.nextUrl;
+            req.nextUrl.href = href.replace(search, '');
+            req.nextUrl.pathname = '/search';
+            req.nextUrl.search = `?s=${s}`;
+            req.nextUrl.searchParams.delete('content');
+            req.nextUrl.searchParams.delete('text');
+
+            return NextResponse.redirect(req.nextUrl);
         } else {
-            return NextResponse.redirect(`${origin}/developer`);
+            req.nextUrl.pathname = '/';
+            return NextResponse.redirect(req.nextUrl);
         }
     }
 
