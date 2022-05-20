545cc6c139c540c75d705fe99a7cd6ac7f0fa315
Amanda J. Henri <ajhenri@mongodb.com>
2022-05-20 18:42:33 -0400

    Add redirect for search page from consistent nav

--- src/pages/_middleware.ts
+++ src/pages/_middleware.ts
@@ -2,7 +2,18 @@ import { NextRequest, NextResponse } from 'next/server';
 import { rewrites } from '../../config/rewrites';
 
 export async function middleware(req: NextRequest) {
-    const { pathname } = req.nextUrl;
+    const { origin, pathname } = req.nextUrl;
+
+    // Handles consistent navigation search as well as
+    // redirect for /learn page.
+    if (pathname == '/learn') {
+        const s = req.nextUrl.searchParams.get('text');
+        if (s && s.length > 0) {
+            return NextResponse.redirect(`${origin}/developer/search?s=${s}`);
+        } else {
+            return NextResponse.redirect(`${origin}/developer`);
+        }
+    }
 
     // Rewrites are done in this middleware due to
     // existing issue with SSG and dynamic routing
