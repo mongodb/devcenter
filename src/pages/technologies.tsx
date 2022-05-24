a09c445629f6bddee5c30cdf29088dd78eda752a
Greg Campoverde <greg.c@mongodb.com>
2022-05-24 18:45:25 -0400

    [DEVHUB-1249] Modify technologies page

--- src/pages/technologies.tsx
+++ src/pages/technologies.tsx
@@ -2,24 +2,14 @@ import { NextPage, GetStaticProps } from 'next';
 import { NextSeo } from 'next-seo';
 import Hero from '../components/hero';
 import { Crumb } from '../components/breadcrumbs/types';
-import {
-    FlashCard,
-    GridLayout,
-    ESingleImageVariant,
-    ImageryType,
-    CTAType,
-    LogoPaths,
-} from '@mdb/flora';
+import { GridLayout, LogoPaths, BrandedIcon, ThirdPartyLogo } from '@mdb/flora';
 import { Grid } from 'theme-ui';
 
 import { technologyToLogo } from '../utils/technology-to-logo';
 import { getAllMetaInfo } from '../service/get-all-meta-info';
 import { MetaInfo } from '../interfaces/meta-info';
-import { getURLPath } from '../utils/format-url-path';
-
-// Temporary until we find a logo to include in Flora.
-const serverlessLogo =
-    'https://webimages.mongodb.com/_com_assets/icons/atlas_serverless.svg';
+import TopicCard from '../components/topic-card';
+import { iconStyles } from '../components/topic-card/styles';
 
 const crumbs: Crumb[] = [
     { text: 'MongoDB Developer Center', url: '/developer' },
@@ -35,66 +25,34 @@ const TechnologiesSection: React.FunctionComponent<{
 }> = ({ technologies }) => {
     return (
         <Grid columns={[1, null, 2, 4]} gap="inc70">
-            {technologies.map(tech => {
-                let imageProps;
+            {technologies.map(({ tagName, slug }) => {
                 const hasLogo =
-                    tech.tagName === 'Serverless' ||
-                    LogoPaths[technologyToLogo[tech.tagName]];
+                    tagName === 'Serverless' ||
+                    LogoPaths[technologyToLogo[tagName]];
+                let icon: JSX.Element | null = null;
                 if (hasLogo) {
-                    // Really annoying, but we have a special case where we use a branded icon for serverless.
-                    if (tech.tagName === 'Serverless') {
-                        imageProps = {
-                            imageConfig: {
-                                src: serverlessLogo,
-                                variant: ESingleImageVariant.NO_RATIO,
-                            },
-                            imageryType: 'image' as ImageryType,
-                        };
+                    if (tagName === 'Serverless') {
+                        icon = (
+                            <BrandedIcon
+                                sx={iconStyles}
+                                name="atlas_serverless"
+                            />
+                        );
                     } else {
-                        imageProps = {
-                            imageConfig: {
-                                src: LogoPaths[technologyToLogo[tech.tagName]],
-                                variant: ESingleImageVariant.NO_RATIO,
-                            },
-                            imageryType: 'image' as ImageryType,
-                        };
+                        icon = (
+                            <ThirdPartyLogo
+                                sx={iconStyles}
+                                variant={technologyToLogo[tagName]}
+                            />
+                        );
                     }
-                } else {
-                    imageProps = { imageryType: 'none' as ImageryType };
                 }
-                const flashCardProps = {
-                    ...imageProps,
-                    cta: {
-                        type: 'link-arrow' as CTAType,
-                        text: 'Learn More',
-                        config: {
-                            href: getURLPath(tech.slug),
-                            linkIconDisableExpand: true, // Doesn't seem to work
-                        },
-                    },
-                    title: tech.tagName,
-                    text: tech.description,
-                    background: false,
-                    alignment: 'left' as 'left',
-                };
                 return (
-                    <FlashCard
-                        key={tech.slug}
-                        {...flashCardProps}
-                        sx={{
-                            boxSizing: 'border-box' as 'border-box',
-                            div: { minHeight: 'unset' },
-                            '>div:first-of-type>div:first-of-type': {
-                                // Janky, but flora doesn't support third party logos in these cards.
-                                width: 64,
-                                height: 64,
-                            },
-                            img: {
-                                width: 64,
-                                height: 64,
-                            },
-                            padding: 0,
-                        }}
+                    <TopicCard
+                        key={tagName}
+                        title={tagName}
+                        href={slug}
+                        icon={icon}
                     />
                 );
             })}
