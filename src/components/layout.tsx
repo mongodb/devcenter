969a66f42fa550c9ec4ce96d9e4db967b60ad972
Matt Colella <matt.colella@mongodb.com>
2022-06-08 14:28:00 -0400

    Refactor consistent nav styling, add devhub default search

--- src/components/layout.tsx
+++ src/components/layout.tsx
@@ -2,15 +2,18 @@ import React from 'react';
 import { Global } from '@emotion/react';
 import { UnifiedFooter } from '@mdb/consistent-nav';
 import { globalStyles, Main } from '../styled/layout';
-
-import ConsistentNav from './consistent-nav';
+import { UnifiedNavCustom } from '../styled/consistent-nav';
 import SecondaryNav from './seconardynavnew/';
 
 const Layout: React.FunctionComponent = ({ children }) => {
     return (
         <>
             <Global styles={globalStyles} />
-            <ConsistentNav />
+            <UnifiedNavCustom
+                position="sticky"
+                floraTheme="default"
+                property={{ name: 'DEVHUB', searchParams: [] }}
+            />
             <SecondaryNav />
             <Main>{children}</Main>
             <UnifiedFooter hideLocale />
