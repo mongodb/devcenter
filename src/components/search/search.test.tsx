268757af260586fd1ddf45141bb427cb7e7433e8
Matt Colella <matt.colella@mongodb.com>
2022-06-15 12:09:39 -0400

    Search unit test

--- src/components/search/search.test.tsx
+++ src/components/search/search.test.tsx
@@ -3,24 +3,57 @@ import '@testing-library/jest-dom';
 
 import Search from '.';
 
-// test('renders search', () => {
-//     render(
-//         <Search />
+test('renders search', () => {
+    render(<Search title="Some Title" placeholder="Search Some Stuff" />);
 
-//     );
+    const title = screen.getByRole('heading', { level: 5 });
+    expect(title).toHaveTextContent('Some Title');
 
-//     const icon = screen.getByTestId('base-icon');
+    const searchInput = screen.getByLabelText('Search Some Stuff');
+    expect(searchInput).toHaveValue('');
+});
 
-//     expect(icon).toHaveAttribute('fill', 'none');
-// });
+test('renders search for code examples', () => {
+    render(
+        <Search
+            title="Some Title"
+            placeholder="Search Some Stuff"
+            contentType="Code Example"
+        />
+    );
 
-// test('renders search for code examples', () => {
-//      render(
-//         <Search />
+    const snippetBox = screen.getByLabelText('Code Snippets');
+    const fullAppBox = screen.getByLabelText('Full Applications');
 
-//     );
+    expect(snippetBox).toHaveAttribute('type', 'checkbox');
+    expect(fullAppBox).toHaveAttribute('type', 'checkbox');
+});
 
-//     const icon = screen.getByTestId('base-icon');
+test('renders search with title link', () => {
+    render(
+        <Search
+            title="Some Title"
+            placeholder="Search Some Stuff"
+            titleLink={{ text: 'All things', href: '/articles/' }}
+        />
+    );
 
-//     expect(icon).not.toHaveAttribute('filled');
-// });
+    const link = screen.getByRole('link');
+    expect(link).toHaveAttribute('href', '/developer/articles/');
+    expect(link.getElementsByTagName('span')[0]).toHaveTextContent(
+        'All things'
+    );
+});
+
+test('renders search with custom element', () => {
+    render(
+        <Search
+            title="Some Title"
+            placeholder="Search Some Stuff"
+            titleElement="h2"
+        />
+    );
+
+    const title = screen.getByRole('heading', { level: 2 });
+    expect(title).toHaveTextContent('Some Title');
+});
