21ff1ef41fafe09e980415809b46deadfea56c48
Amanda J. Henri <ajhenri@mongodb.com>
2022-06-24 12:26:20 -0400

    Complete author pages SSR conversion

--- src/api-requests/get-authors.ts
+++ src/api-requests/get-authors.ts
@@ -2,35 +2,85 @@ import { Author } from '../interfaces/author';
 import { UnderlyingClient } from '../types/client-factory';
 import { ApolloQueryResult, gql } from '@apollo/client';
 
+export const authorFields = `
+    name
+    bio
+    image {
+        url
+    }
+    location
+    title
+    linkedin
+    facebook
+    twitter
+    youtube
+    calculated_slug
+    articles: new_articles {
+        description
+        authors {
+            name
+            bio
+            image {
+                url
+            }
+            calculated_slug
+            twitter
+        }
+        slug
+        image {
+            url
+        }
+        title: name
+        publishDate: published_at
+        originalPublishDate
+        updateDate: updatedAt
+        otherTags: other_tags {
+            l1Product: l_1_product {
+                name
+                calculatedSlug: calculated_slug
+            }
+            l2Product: l_2_product {
+                name
+                calculatedSlug: calculated_slug
+            }
+            programmingLanguage: programming_language {
+                name
+                calculatedSlug: calculated_slug
+            }
+            technology: technology {
+                name
+                calculatedSlug: calculated_slug
+            }
+            contentType: content_type {
+                contentType: content_type
+                calculatedSlug: calculated_slug
+            }
+            spokenLanguage: spoken_language {
+                name
+                calculatedSlug: calculated_slug
+            }
+            expertiseLevel: expertise_level {
+                name: level
+                calculatedSlug: calculated_slug
+            }
+            authorType: author_type {
+                name
+                calculatedSlug: calculated_slug
+            }
+        }
+        calculatedSlug: calculated_slug
+    }
+    podcasts
+    videos: new_videos
+`;
+
 export const getAllAuthorsFromAPI = async (
     client: UnderlyingClient<'ApolloREST'>
 ): Promise<Author[]> => {
     const query = gql`
         query Authors {
             authors @rest(type: "Authors", path: "/authors") {
-                name
-                bio
-                image {
-                    url
-                }
-                location
-                title
-                linkedin
-                facebook
-                twitter
-                youtube
-                calculated_slug
-                articles: new_articles {
-                    name
-                    calculated_slug
-                    description
-                    otherTags: other_tags(first: 1) {
-                        contentType: content_type(first: 1) {
-                            contentType: content_type
-                            calculatedSlug: calculated_slug
-                        }
-                    }
-                }
+                ${authorFields}
             }
         }
     `;
@@ -38,4 +88,21 @@ export const getAllAuthorsFromAPI = async (
         await client.query({ query });
 
     return data.authors;
+};
+
+export const getAuthorFromAPI = async (
+    client: UnderlyingClient<'ApolloREST'>,
+    calculatedSlug: string
+): Promise<Author | null> => {
+    const query = gql`
+        query Authors {
+            authors @rest(type: "Authors", path: "/authors?calculated_slug_eq=${calculatedSlug}") {
+                ${authorFields}
+            }
+        }
+    `;
+    const { data }: ApolloQueryResult<{ authors: Author[] }> =
+        await client.query({ query });
+
+    return data.authors.length > 0 ? data.authors[0] : null;
 };
--- src/pages/author/[...slug].tsx
+++ src/pages/author/[...slug].tsx
@@ -1,12 +1,15 @@
-import type { NextPage, GetStaticProps } from 'next';
+import type {
+    NextPage,
+    GetServerSideProps,
+    GetServerSidePropsContext,
+} from 'next';
 import { NextSeo } from 'next-seo';
 import { ParsedUrlQuery } from 'querystring';
-import { getAllAuthors } from '../../service/get-all-authors';
+import { getAuthor } from '../../service/get-all-authors';
 import { Author, Image } from '../../interfaces/author';
+import { flattenTags } from '../../utils/flatten-tags';
 import { Button, GridLayout, SpeakerLockup, TypographyScale } from '@mdb/flora';
-import React, { useState } from 'react';
 import Card, { getCardProps } from '../../components/card';
-import { getAllContentItems } from '../../service/get-all-content';
 import { ContentItem } from '../../interfaces/content-item';
 import { Grid } from 'theme-ui';
 import { getPlaceHolderImage } from '../../utils/get-place-holder-thumbnail';
@@ -258,25 +261,41 @@ interface IParams extends ParsedUrlQuery {
     slug: string[];
 } // Need this to avoid TS errors.
 
-export const getServerSideProps = async (context: any) => {
+export const getServerSideProps: GetServerSideProps = async (
+    context: GetServerSidePropsContext
+) => {
     const { slug } = context.params as IParams;
     const slugString = '/author/' + slug[0];
 
-    const authors: Author[] = await getAllAuthors();
+    const author: Author | null = await getAuthor(slugString);
+    if (!author) {
+        return {
+            notFound: true,
+        };
+    }
 
-    const author = authors.filter(
-        a => a.calculated_slug.toLowerCase() === slugString.toLowerCase()
-    )[0];
-
-    const articlesByAuthor = author.articles
-        ? author.articles.map(articles => articles.name)
-        : [];
-
-    const allContent: ContentItem[] = await getAllContentItems(); // TODO: Refactor
-
-    const filteredContent = allContent.filter(a =>
-        articlesByAuthor.includes(a.title)
-    );
+    let authorContentItems: any[] = [];
+    for (const article of author.articles as any) {
+        const item: ContentItem = {
+            collectionType: 'Article',
+            authors: article.authors,
+            category: article.otherTags[0].contentType.contentType,
+            contentDate: article.originalPublishDate || article.publishDate,
+            description: article.description,
+            slug: article.calculatedSlug.startsWith('/')
+                ? article.calculatedSlug.substring(1)
+                : article.calculatedSlug,
+            tags: flattenTags(article.otherTags),
+            title: article.title,
+        };
+        if (article.image) {
+            item.image = {
+                url: article.image.url,
+                alt: article.image.alt || 'random alt',
+            };
+        }
+        authorContentItems.push(item);
+    }
 
     const data = {
         name: author.name,
@@ -288,7 +307,7 @@ export const getServerSideProps = async (context: any) => {
         facebook: author.facebook,
         twitter: author.twitter,
         youtube: author.youtube,
-        articles: filteredContent ? filteredContent : [],
+        articles: authorContentItems,
         calculated_slug: author.calculated_slug,
     };
 
