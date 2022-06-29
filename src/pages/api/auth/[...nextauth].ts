8c346c898319d21e30b9d927608b8bc463c437c9
harika.kotipalli <harika.1818@gmail.com>
2022-06-29 09:44:40 -0400

    Adding github auth

--- src/pages/api/auth/[...nextauth].ts
+++ src/pages/api/auth/[...nextauth].ts
@@ -4,8 +4,8 @@ import NextAuth from 'next-auth';
 export default NextAuth({
     providers: [
         GithubProvider({
-            clientId: 'afc4427410c1263c56d4',
-            clientSecret: 'e197d4e6acc5448415e4422b421ec28cc8ea04b6',
+            clientId: process.env.CLIENT_ID,
+            clientSecret: process.env.CLIENT_SECRET,
         }),
     ],
 });
