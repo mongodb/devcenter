931dc413fd69d7a3afec4dd9a75728736b9cf808
Matt Colella <matt.colella@mongodb.com>
2022-07-06 15:46:19 -0400

    trigger webhook

--- src/pages/api/preview-build-webhook.ts
+++ src/pages/api/preview-build-webhook.ts
@@ -1,6 +1,20 @@
 import type { NextApiRequest, NextApiResponse } from 'next';
+import axios from 'axios';
 import logger from '../../utils/logger';
 import { verifyPayloadSigntaure } from '../../utils/verify-payload-signature';
+
+const triggerWebhook = async (body: any) => {
+    if (['closed', 'reopened'].includes(body.action)) {
+        const branch = encodeURIComponent(body['pull_request'].head.ref);
+        const droneEndpoint = `https://drone.corp.mongodb.com/api/repos/mongodb/devcenter/builds?branch=${branch}&number=${body.number}&action=${body.action}`;
+        console.log('ENDPOINT', droneEndpoint);
+        const headers = {
+            Authorization: `Bearer ${process.env['DRONE_TOKEN']}`,
+        };
+        const resp = axios.post(droneEndpoint, {}, { headers });
+        console.log(resp);
+    }
+};
 
 export default async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'POST') {
@@ -8,8 +22,8 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
     }
 
     const event = req.headers['x-github-event'];
-    if (!event || Array.isArray(event) || event !== 'ping') {
-        // if (!event || Array.isArray(event) || event !== 'pull-request') {
+    // if (!event || Array.isArray(event) || event !== 'ping') {
+    if (!event || Array.isArray(event) || event !== 'pull_request') {
         return res.status(400).send('Event must be pull-requst');
     }
 
@@ -25,5 +39,6 @@ export default async (req: NextApiRequest, res: NextApiResponse) => {
         return res.status(400).send('Bad Signature');
     }
 
+    await triggerWebhook(req.body);
     return res.status(200).send('Successfully triggered preview build.');
 };
