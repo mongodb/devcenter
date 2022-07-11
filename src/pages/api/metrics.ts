e34dfe5ad14532ec323b57e4d698800a32aaea4d
Amanda J. Henri <ajhenri@mongodb.com>
2022-07-11 17:04:45 -0400

    Decompose getRegistry, update email for alerts

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -1,8 +1,9 @@
 import { NextApiResponse } from 'next';
+import { Registry } from 'prom-client';
 import { prometheus } from '../../config/prometheus';
 
 const metricsApi = async (_: any, res: NextApiResponse) => {
-    const register = prometheus.getRegistry();
+    const register = prometheus.getRegistry() as Registry;
     res.setHeader('Content-type', register.contentType);
     res.send(await register.metrics());
 };
