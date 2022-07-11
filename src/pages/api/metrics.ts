5a04199d93385f7ec533121d6a96a6c3af77b01f
Amanda J. Henri <ajhenri@mongodb.com>
2022-07-11 19:02:12 -0400

    Log env vars

--- src/pages/api/metrics.ts
+++ src/pages/api/metrics.ts
@@ -1,15 +1,20 @@
 import { NextApiRequest, NextApiResponse } from 'next';
 import { Registry } from 'prom-client';
 import { prometheus } from '../../config/prometheus';
+import logger from '../../utils/logger';
 
 const metricsApi = async (req: NextApiRequest, res: NextApiResponse) => {
     const authHeader = req.headers.authorization;
+    logger.info(authHeader);
     if (!authHeader) return res.status(400).send('Bad Request');
 
     const authValue = authHeader.split(' ')[1];
     const [username, password] = Buffer.from(authValue, 'base64')
         .toString('utf8')
         .split(':');
+    logger.info([username, password]);
+    logger.info(process.env.PROMETHEUS_AUTH_USER);
+    logger.info(process.env.PROMETHEUS_AUTH_PASS);
     if (
         process.env.PROMETHEUS_AUTH_USER === username &&
         process.env.PROMETHEUS_AUTH_PASS === password
