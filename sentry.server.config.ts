b04528379bedc21de6fe68477c84f0665b99a779
Amanda J. Henri <ajhenri@mongodb.com>
2022-07-26 10:55:02 -0400

    More debugging logs

--- sentry.server.config.ts
+++ sentry.server.config.ts
@@ -2,6 +2,9 @@ import * as Sentry from '@sentry/nextjs';
 
 const SENTRY_DSN: string | undefined = process.env.SENTRY_DSN;
 
+console.log('server config');
+console.log('process.env.APP_ENV', process.env.APP_ENV);
+
 Sentry.init({
     dsn: SENTRY_DSN,
     environment: process.env.APP_ENV ? process.env.APP_ENV : 'dev',
