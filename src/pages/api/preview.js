cb17d044d23cf67e67df0b0c65b5c4ddb028fa4e
Amanda J. Henri <ajhenri@mongodb.com>
2022-07-26 08:44:11 -0400

    Test Sentry

--- src/pages/api/preview.js
+++ src/pages/api/preview.js
@@ -1,9 +1,14 @@
-export default function handler(req, res) {
-    if (
-        req.query.secret !== process.env.PREVIEW_SECRET_KEY ||
-        !req.query.redirect
-    ) {
-        return res.status(401).json({ message: 'Invalid token' });
-    }
-    res.redirect('/developer/preview' + req.query.redirect);
-}
+import { withSentry } from '@sentry/nextjs';
+
+const previewHandler = function handler(req, res) {
+    throw new Error('test error');
+    // if (
+    //     req.query.secret !== process.env.PREVIEW_SECRET_KEY ||
+    //     !req.query.redirect
+    // ) {
+    //     return res.status(401).json({ message: 'Invalid token' });
+    // }
+    // res.redirect('/developer/preview' + req.query.redirect);
+};
+
+export default withSentry(previewHandler);
