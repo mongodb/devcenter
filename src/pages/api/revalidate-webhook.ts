28ff403c618edf73fd15b2d3691da57e916dcf16
Amanda J. Henri <ajhenri@mongodb.com>
2022-08-02 15:37:53 -0400

    Add rate-limiting and increased error handling on revalidate-webhook, based on updates on educators

--- src/pages/api/revalidate-webhook.ts
+++ src/pages/api/revalidate-webhook.ts
@@ -1,10 +1,30 @@
 import * as Sentry from '@sentry/nextjs';
 import type { NextApiRequest, NextApiResponse } from 'next';
-import getDynamicPaths from '../../service/get-dynamic-paths';
+import rateLimit from '../../utils/rate-limit';
+
+const limiter = rateLimit({
+    max: 600, // cache limit of 600 per 30 second period.
+    ttl: 30 * 1000,
+});
+
+const MAX_REQUESTS_PER_PERIOD = 10;
 
 const revalidateHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (req.method !== 'POST') {
         return res.status(405).send('Method not allowed');
+    }
+
+    if (!process.env.REVALIDATE_TOKEN)
+        return res.status(500).send('Something went wrong');
+
+    try {
+        await limiter.check(
+            res,
+            MAX_REQUESTS_PER_PERIOD,
+            req.socket.remoteAddress || ''
+        );
+    } catch {
+        return res.status(429).send('Too many requests');
     }
 
     const { body } = req;
@@ -13,17 +33,12 @@ const revalidateHandler = async (req: NextApiRequest, res: NextApiResponse) => {
     if (token !== process.env.REVALIDATE_TOKEN)
         return res.status(401).send('Unauthorized');
 
+    if (!body.slug) {
+        return res.status(400).send('Bad Request');
+    }
+
     try {
-        if (body.slug) {
-            await res.revalidate(`/developer${body.slug}`);
-        } else if (body.all) {
-            const dynamicPaths = await getDynamicPaths();
-            for (const path of dynamicPaths) {
-                const { params } = path;
-                const slug = params.slug.join('/');
-                await res.revalidate(`/developer/${slug}`);
-            }
-        }
+        await res.revalidate(`/developer${body.slug}`);
         return res.json({ revalidated: true });
     } catch (err) {
         Sentry.captureException(err);
