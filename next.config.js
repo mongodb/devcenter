3230869ba811ac565714c41af10ac9bf2298cdca
Amanda J. Henri <ajhenri@mongodb.com>
2022-08-29 11:37:42 -0400

    Update cache-control headers

--- next.config.js
+++ next.config.js
@@ -34,7 +34,7 @@ const configVals = {
                 headers: [
                     {
                         key: 'Cache-Control',
-                        value: 'max-age=3600',
+                        value: 'max-age=3600, stale-while-revalidate=120',
                     },
                     {
                         key: 'Strict-Transport-Security',
