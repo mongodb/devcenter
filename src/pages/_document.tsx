34ca56f4f41473f6c1fd469ae31c72fa0f0623bc
Amanda J. Henri <ajhenri@mongodb.com>
2022-09-22 14:53:37 -0400

    Revert iframe update (#319)

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -36,11 +36,14 @@ export default class MyDocument extends Document {
                     ></link>
                 </Head>
                 <body>
-                    <noscript
-                        dangerouslySetInnerHTML={{
-                            __html: `<iframe src="https://www.googletagmanager.com/ns.html?id=${GTM_ID}" height="0" width="0" style="display: none; visibility: hidden;" />`,
-                        }}
-                    />
+                    <noscript>
+                        <iframe
+                            src={`https://www.googletagmanager.com/ns.html?id=${GTM_ID}`}
+                            height="0"
+                            width="0"
+                            style={{ display: 'none', visibility: 'hidden' }}
+                        />
+                    </noscript>
                     <Main />
                     <NextScript />
                     <Script
