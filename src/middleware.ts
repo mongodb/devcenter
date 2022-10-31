59307e79bf817dfc368bb193f1cfdb57616a5afc
Amanda J. Henri <ajhenri@mongodb.com>
2022-10-31 18:03:47 -0400

    Debug issue discussed in Kanopy channel

--- src/middleware.ts
+++ src/middleware.ts
@@ -3,107 +3,110 @@ import { NextRequest, NextResponse, userAgent } from 'next/server';
 import { rewrites } from '../config/rewrites';
 import { logRequestData } from './utils/logger';
 
+// Remove everything else in order to debug, ensuring that the root cause
+// of the issue is not due to the code below.
+
 const middleware = async (req: NextRequest) => {
-    const { pathname } = req.nextUrl;
+    // const { pathname } = req.nextUrl;
 
-    const origin = req.headers.get('Origin') || '';
+    // const origin = req.headers.get('Origin') || '';
 
-    const host = process.env.VERCEL_URL
-        ? process.env.VERCEL_URL
-        : process.env.HOST_URL;
+    // const host = process.env.VERCEL_URL
+    //     ? process.env.VERCEL_URL
+    //     : process.env.HOST_URL;
 
-    const checkRequest = // Only attempt to block POST or PUT API requests (feedback and request content).
-        pathname.startsWith('/api/') &&
-        !pathname.includes('webhook') &&
-        ['POST', 'PUT'].includes(req.method);
+    // const checkRequest = // Only attempt to block POST or PUT API requests (feedback and request content).
+    //     pathname.startsWith('/api/') &&
+    //     !pathname.includes('webhook') &&
+    //     ['POST', 'PUT'].includes(req.method);
 
-    if (checkRequest) {
-        if (
-            origin.replace(/^(https?:|)\/\//, '') !== host // Remove the protocol from the URL.
-        ) {
-            const ua: any = userAgent(req);
+    // if (checkRequest) {
+    //     if (
+    //         origin.replace(/^(https?:|)\/\//, '') !== host // Remove the protocol from the URL.
+    //     ) {
+    //         const ua: any = userAgent(req);
 
-            console.log(
-                `${req.ip} blocked because of bad user agent (${ua?.browser?.name}) or origin (${origin})`
-            );
+    //         console.log(
+    //             `${req.ip} blocked because of bad user agent (${ua?.browser?.name}) or origin (${origin})`
+    //         );
 
-            const blockedURL = new URL('/api/blocked', req.url);
-            blockedURL.searchParams.set('from', req.nextUrl.pathname);
+    //         const blockedURL = new URL('/api/blocked', req.url);
+    //         blockedURL.searchParams.set('from', req.nextUrl.pathname);
 
-            const res = NextResponse.rewrite(blockedURL);
-            logRequestData(pathname, req.method, res.status);
-            return res;
-        }
+    //         const res = NextResponse.rewrite(blockedURL);
+    //         logRequestData(pathname, req.method, res.status);
+    //         return res;
+    //     }
 
-        const res = NextResponse.next();
-        res.headers.set('Content-Type', 'application/json');
+    //     const res = NextResponse.next();
+    //     res.headers.set('Content-Type', 'application/json');
 
-        logRequestData(pathname, req.method, res.status);
-        return res;
-    }
+    //     logRequestData(pathname, req.method, res.status);
+    //     return res;
+    // }
 
-    // Handles consistent navigation search as well as
-    // redirect for /learn page.
-    if (pathname == '/learn/') {
-        const { searchParams } = req.nextUrl;
-        const s = searchParams.get('text');
-        if (s && s.length > 0) {
-            const { href, search } = req.nextUrl;
-            req.nextUrl.href = href.replace(search, '');
-            req.nextUrl.pathname = '/search';
-            req.nextUrl.search = `?s=${s}`;
-            req.nextUrl.searchParams.delete('content');
-            req.nextUrl.searchParams.delete('text');
+    // // Handles consistent navigation search as well as
+    // // redirect for /learn page.
+    // if (pathname == '/learn/') {
+    //     const { searchParams } = req.nextUrl;
+    //     const s = searchParams.get('text');
+    //     if (s && s.length > 0) {
+    //         const { href, search } = req.nextUrl;
+    //         req.nextUrl.href = href.replace(search, '');
+    //         req.nextUrl.pathname = '/search';
+    //         req.nextUrl.search = `?s=${s}`;
+    //         req.nextUrl.searchParams.delete('content');
+    //         req.nextUrl.searchParams.delete('text');
 
-            const res = NextResponse.redirect(req.nextUrl);
-            logRequestData(pathname, req.method, res.status);
-            return res;
-        } else if (
-            searchParams.get('products') === 'Mobile' ||
-            searchParams.get('products') === 'Realm'
-        ) {
-            if (searchParams.get('content') === 'articles') {
-                req.nextUrl.pathname = '/products/realm/articles/';
-            } else {
-                req.nextUrl.pathname = '/products/realm/';
-            }
-            const res = NextResponse.redirect(req.nextUrl);
-            logRequestData(pathname, req.method, res.status);
-            return res;
-        } else {
-            req.nextUrl.pathname = '/';
-            const res = NextResponse.redirect(req.nextUrl);
-            logRequestData(pathname, req.method, res.status);
-            return res;
-        }
-    }
+    //         const res = NextResponse.redirect(req.nextUrl);
+    //         logRequestData(pathname, req.method, res.status);
+    //         return res;
+    //     } else if (
+    //         searchParams.get('products') === 'Mobile' ||
+    //         searchParams.get('products') === 'Realm'
+    //     ) {
+    //         if (searchParams.get('content') === 'articles') {
+    //             req.nextUrl.pathname = '/products/realm/articles/';
+    //         } else {
+    //             req.nextUrl.pathname = '/products/realm/';
+    //         }
+    //         const res = NextResponse.redirect(req.nextUrl);
+    //         logRequestData(pathname, req.method, res.status);
+    //         return res;
+    //     } else {
+    //         req.nextUrl.pathname = '/';
+    //         const res = NextResponse.redirect(req.nextUrl);
+    //         logRequestData(pathname, req.method, res.status);
+    //         return res;
+    //     }
+    // }
 
-    // Rewrites are done in this middleware due to
-    // existing issue with SSG and dynamic routing
-    // when using built-in rewrites configuration.
-    for (const rewrite of rewrites) {
-        let destination: string | null = null;
+    // // Rewrites are done in this middleware due to
+    // // existing issue with SSG and dynamic routing
+    // // when using built-in rewrites configuration.
+    // for (const rewrite of rewrites) {
+    //     let destination: string | null = null;
 
-        if (rewrite.regex) {
-            const regex = new RegExp(rewrite.regex);
-            if (regex.test(pathname)) {
-                destination = `${process.env.DEVHUB_URL}${pathname}`;
-            }
-        }
+    //     if (rewrite.regex) {
+    //         const regex = new RegExp(rewrite.regex);
+    //         if (regex.test(pathname)) {
+    //             destination = `${process.env.DEVHUB_URL}${pathname}`;
+    //         }
+    //     }
 
-        if (pathname == rewrite.source || pathname == `${rewrite.source}/`) {
-            destination = rewrite.destination;
-        }
+    //     if (pathname == rewrite.source || pathname == `${rewrite.source}/`) {
+    //         destination = rewrite.destination;
+    //     }
 
-        if (destination) {
-            const res = NextResponse.rewrite(destination);
-            logRequestData(pathname, req.method, res.status);
-            return res;
-        }
-    }
+    //     if (destination) {
+    //         const res = NextResponse.rewrite(destination);
+    //         logRequestData(pathname, req.method, res.status);
+    //         return res;
+    //     }
+    // }
 
     const res = NextResponse.next();
-    logRequestData(pathname, req.method, res.status);
+    // logRequestData(pathname, req.method, res.status);
 
     return res;
 };
--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -22,7 +22,7 @@ const DynamicContentPage: NextPage<ContentPageProps> = ({
     pageType,
     pageData,
 }) => {
-    return <DynamicContentTemplate pageType={pageType} pageData={pageData} />;
+    return <p>Debug</p>;
 };
 
 export default DynamicContentPage;
@@ -30,73 +30,75 @@ export default DynamicContentPage;
 export const getServerSideProps: GetServerSideProps = async (
     context: GetServerSidePropsContext
 ) => {
-    const { query } = context;
-    const { slug } = query as PageParams;
+    // Remove everything else in order to debug, ensuring that the root cause
+    // of the issue is not due to the code below.
+    const delay = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
+    await delay(3000);
 
-    const dynamicPageType: DynamicPageType = await pageTypeFactory(slug);
-    const {
-        pageType,
-        pageParams,
-    }: { pageType: PageType; pageParams: PageParams } = dynamicPageType;
-    let data: any | null = {};
+    // const { query } = context;
+    // const { slug } = query as PageParams;
 
-    let pageNumber: number;
-    switch (pageType) {
-        case PageType.Content:
-            // cache content only for 5 minutes
-            context.res.setHeader(
-                'Cache-Control',
-                'public, must-revalidate, proxy-revalidate, max-age=300'
-            );
+    // const dynamicPageType: DynamicPageType = await pageTypeFactory(slug);
+    // const {
+    //     pageType,
+    //     pageParams,
+    // }: { pageType: PageType; pageParams: PageParams } = dynamicPageType;
+    // let data: any | null = {};
 
-            data = await getContentPageData(slug);
-            break;
-        case PageType.Topic:
-            pageNumber = parsePageNumber(query.page);
-            data = await getTopicPageData(
-                pageParams.l1_l2 as string,
-                pageParams.slug,
-                pageNumber
-            );
-            if (
-                data?.initialSearchContent &&
-                !isValidPage(data?.initialSearchContent.length, pageNumber)
-            ) {
-                return {
-                    notFound: true,
-                };
-            }
-            break;
-        case PageType.TopicContentType:
-            pageNumber = parsePageNumber(query.page);
-            data = await getTopicContentTypePageData(
-                pageParams.l1_l2 as string,
-                pageParams.topic as string,
-                pageParams.slug,
-                pageNumber
-            );
-            if (
-                data?.initialSearchContent &&
-                !isValidPage(data?.initialSearchContent.length, pageNumber)
-            ) {
-                return {
-                    notFound: true,
-                };
-            }
-            break;
-        default:
-            data = null;
-            break;
-    }
+    // let pageNumber: number;
+    // switch (pageType) {
+    //     case PageType.Content:
+    //         // cache content only for 5 minutes
+    //         context.res.setHeader(
+    //             'Cache-Control',
+    //             'public, must-revalidate, proxy-revalidate, max-age=300'
+    //         );
 
-    if (!pageType || !data || Object.keys(data).length === 0) {
-        return { notFound: true };
-    }
+    //         data = await getContentPageData(slug);
+    //         break;
+    //     case PageType.Topic:
+    //         pageNumber = parsePageNumber(query.page);
+    //         data = await getTopicPageData(
+    //             pageParams.l1_l2 as string,
+    //             pageParams.slug,
+    //             pageNumber
+    //         );
+    //         if (
+    //             data?.initialSearchContent &&
+    //             !isValidPage(data?.initialSearchContent.length, pageNumber)
+    //         ) {
+    //             return {
+    //                 notFound: true,
+    //             };
+    //         }
+    //         break;
+    //     case PageType.TopicContentType:
+    //         pageNumber = parsePageNumber(query.page);
+    //         data = await getTopicContentTypePageData(
+    //             pageParams.l1_l2 as string,
+    //             pageParams.topic as string,
+    //             pageParams.slug,
+    //             pageNumber
+    //         );
+    //         if (
+    //             data?.initialSearchContent &&
+    //             !isValidPage(data?.initialSearchContent.length, pageNumber)
+    //         ) {
+    //             return {
+    //                 notFound: true,
+    //             };
+    //         }
+    //         break;
+    //     default:
+    //         data = null;
+    //         break;
+    // }
+
+    // if (!pageType || !data || Object.keys(data).length === 0) {
+    //     return { notFound: true };
+    // }
 
     return {
-        props: {
-            pageData: data,
-            pageType: pageType,
-        },
+        props: {},
     };
 };
