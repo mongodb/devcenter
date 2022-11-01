4c8ff47905834e2ef2d8605ba2f36c5ed425513f
Amanda J. Henri <ajhenri@mongodb.com>
2022-11-01 09:26:01 -0400

    Revert "Debug issue discussed in Kanopy channel"
    This reverts commit 59307e79bf817dfc368bb193f1cfdb57616a5afc.

--- src/pages/[...slug].tsx
+++ src/pages/[...slug].tsx
@@ -22,7 +22,7 @@ const DynamicContentPage: NextPage<ContentPageProps> = ({
     pageType,
     pageData,
 }) => {
-    return <p>Debug</p>;
+    return <DynamicContentTemplate pageType={pageType} pageData={pageData} />;
 };
 
 export default DynamicContentPage;
@@ -30,75 +30,73 @@ export default DynamicContentPage;
 export const getServerSideProps: GetServerSideProps = async (
     context: GetServerSidePropsContext
 ) => {
-    // Remove everything else in order to debug, ensuring that the root cause
-    // of the issue is not due to the code below.
-    const delay = (ms: any) => new Promise(resolve => setTimeout(resolve, ms));
-    await delay(3000);
+    const { query } = context;
+    const { slug } = query as PageParams;
 
-    // const { query } = context;
-    // const { slug } = query as PageParams;
+    const dynamicPageType: DynamicPageType = await pageTypeFactory(slug);
+    const {
+        pageType,
+        pageParams,
+    }: { pageType: PageType; pageParams: PageParams } = dynamicPageType;
+    let data: any | null = {};
 
-    // const dynamicPageType: DynamicPageType = await pageTypeFactory(slug);
-    // const {
-    //     pageType,
-    //     pageParams,
-    // }: { pageType: PageType; pageParams: PageParams } = dynamicPageType;
-    // let data: any | null = {};
+    let pageNumber: number;
+    switch (pageType) {
+        case PageType.Content:
+            // cache content only for 5 minutes
+            context.res.setHeader(
+                'Cache-Control',
+                'public, must-revalidate, proxy-revalidate, max-age=300'
+            );
 
-    // let pageNumber: number;
-    // switch (pageType) {
-    //     case PageType.Content:
-    //         // cache content only for 5 minutes
-    //         context.res.setHeader(
-    //             'Cache-Control',
-    //             'public, must-revalidate, proxy-revalidate, max-age=300'
-    //         );
+            data = await getContentPageData(slug);
+            break;
+        case PageType.Topic:
+            pageNumber = parsePageNumber(query.page);
+            data = await getTopicPageData(
+                pageParams.l1_l2 as string,
+                pageParams.slug,
+                pageNumber
+            );
+            if (
+                data?.initialSearchContent &&
+                !isValidPage(data?.initialSearchContent.length, pageNumber)
+            ) {
+                return {
+                    notFound: true,
+                };
+            }
+            break;
+        case PageType.TopicContentType:
+            pageNumber = parsePageNumber(query.page);
+            data = await getTopicContentTypePageData(
+                pageParams.l1_l2 as string,
+                pageParams.topic as string,
+                pageParams.slug,
+                pageNumber
+            );
+            if (
+                data?.initialSearchContent &&
+                !isValidPage(data?.initialSearchContent.length, pageNumber)
+            ) {
+                return {
+                    notFound: true,
+                };
+            }
+            break;
+        default:
+            data = null;
+            break;
+    }
 
-    //         data = await getContentPageData(slug);
-    //         break;
-    //     case PageType.Topic:
-    //         pageNumber = parsePageNumber(query.page);
-    //         data = await getTopicPageData(
-    //             pageParams.l1_l2 as string,
-    //             pageParams.slug,
-    //             pageNumber
-    //         );
-    //         if (
-    //             data?.initialSearchContent &&
-    //             !isValidPage(data?.initialSearchContent.length, pageNumber)
-    //         ) {
-    //             return {
-    //                 notFound: true,
-    //             };
-    //         }
-    //         break;
-    //     case PageType.TopicContentType:
-    //         pageNumber = parsePageNumber(query.page);
-    //         data = await getTopicContentTypePageData(
-    //             pageParams.l1_l2 as string,
-    //             pageParams.topic as string,
-    //             pageParams.slug,
-    //             pageNumber
-    //         );
-    //         if (
-    //             data?.initialSearchContent &&
-    //             !isValidPage(data?.initialSearchContent.length, pageNumber)
-    //         ) {
-    //             return {
-    //                 notFound: true,
-    //             };
-    //         }
-    //         break;
-    //     default:
-    //         data = null;
-    //         break;
-    // }
-
-    // if (!pageType || !data || Object.keys(data).length === 0) {
-    //     return { notFound: true };
-    // }
+    if (!pageType || !data || Object.keys(data).length === 0) {
+        return { notFound: true };
+    }
 
     return {
-        props: {},
+        props: {
+            pageData: data,
+            pageType: pageType,
+        },
     };
 };
