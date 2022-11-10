bf68378f289f0fc24ce35e8f35b4eb9ed3503db0
Amanda J. Henri <ajhenri@mongodb.com>
2022-11-10 15:11:52 -0500

    [DEVHUB-1554] [DEVHUB-1555] Remove rewrites of images, other files and remote work tag

--- config/rewrites.js
+++ config/rewrites.js
@@ -7,14 +7,6 @@ need to be modified before setting the rewrites attribute in the next.config.js.
 
 const rewrites = [
     {
-        source: '/article/10-tips-making-remote-work-actually-work/',
-        destination: `${process.env.DEVHUB_URL}/article/10-tips-making-remote-work-actually-work/`,
-    },
-    {
-        source: '/article/10-ways-raise-your-profile-developer/',
-        destination: `${process.env.DEVHUB_URL}/article/10-ways-raise-your-profile-developer/`,
-    },
-    {
         source: '/article/behind-scenes-mongodb-podcast/',
         destination: `${process.env.DEVHUB_URL}/article/behind-scenes-mongodb-podcast/`,
     },
@@ -97,95 +89,6 @@ const rewrites = [
     {
         source: '/community-champions/rodrigo-nascimento/',
         destination: `${process.env.DEVHUB_URL}/community-champions/rodrigo-nascimento/`,
-    },
-    {
-        source: '/tag/remote-work/',
-        destination: `${process.env.DEVHUB_URL}/tag/remote-work/`,
-    },
-    {
-        source: '/:file.js',
-        destination: `${process.env.DEVHUB_URL}/:file.js`,
-        regex: '^/(.+)\\.js$',
-    },
-    {
-        source: '/page-data/:data*.json',
-        destination: `${process.env.DEVHUB_URL}/page-data/:data*.json`,
-        regex: '^/page-data/(.+)\\.json$',
-    },
-    {
-        source: '/static/:file.(png|svg|woff|woff2)',
-        destination: `${process.env.DEVHUB_URL}/static/:file.png`,
-        regex: '^/static/(.+)\\.(png|svg|woff|woff2)$',
-    },
-    {
-        source: '/images/bios/:image.jpg',
-        destination: `${process.env.DEVHUB_URL}/images/bios/:image.jpg`,
-        regex: '^/images/bios/(.+)\\.jpg$',
-    },
-    {
-        source: '/images/community/:image.jpg',
-        destination: `${process.env.DEVHUB_URL}/images/community/:image.jpg`,
-        regex: '^/images/community/(.+)\\.jpg$',
-    },
-    {
-        source: '/static/:image.jpg',
-        destination: `${process.env.DEVHUB_URL}/static/:image.jpg`,
-        regex: '^/static/(.+)\\.jpg$',
-    },
-    {
-        source: '/images/atf-images/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/atf-images/:image*.png`,
-        regex: '^/images/atf-images/(.+)\\.png$',
-    },
-    {
-        source: '/images/how-to/:image*.(png|jpg|jpeg|gif)',
-        destination: `${process.env.DEVHUB_URL}/images/how-to/:image*.(png|jpg|jpeg|gif)`,
-        regex: '^/images/how-to/(.+)\\.(png|jpg|jpeg|gif)$',
-    },
-    {
-        source: '/images/article/:image*.(png|jpg|jpeg|gif)',
-        destination: `${process.env.DEVHUB_URL}/images/article/:image*.(png|jpg|jpeg|gif)`,
-        regex: '^/images/article/(.+)\\.(png|jpg|jpeg|gif)$',
-    },
-    {
-        source: '/images/qs-badges/:image*.(png|jpg|jpeg|gif)',
-        destination: `${process.env.DEVHUB_URL}/images/qs-badges/:image*.(png|jpg|jpeg|gif)`,
-        regex: '^/images/qs-badges/(.+)\\.(png|jpg|jpeg|gif)$',
-    },
-    {
-        source: '/images/starlette/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/starlette/:image*.png`,
-        regex: '^/images/starlette/(.+)\\.png$',
-    },
-    {
-        source: '/images/heroku/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/heroku/:image*.png`,
-        regex: '^/images/heroku/(.+)\\.png$',
-    },
-    {
-        source: '/images/aws-lambda/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/aws-lambda/:image*.png`,
-        regex: '^/images/aws-lambda/(.+)\\.png$',
-    },
-    {
-        source: '/images/aws-cloudformation/:image*.(gif|png)',
-        destination: `${process.env.DEVHUB_URL}/images/aws-cloudformation/:image*.(gif|png)`,
-        regex: '^/images/aws-cloudformation/(.+)\\.(gif|png)$',
-    },
-    {
-        source: '/images/atlas-fts/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/atlas-fts/:image*.png`,
-        regex: '^/images/atlas-fts/(.+)\\.png$',
-    },
-    {
-        source: '/images/charts-sdk/:image*.png',
-        destination: `${process.env.DEVHUB_URL}/images/charts-sdk/:image*.png`,
-        regex: '^/images/charts-sdk/(.+)\\.png$',
-    },
-    {
-        source: '/images/:image*.(png|jpg|jpeg|gif)',
-        destination: `${process.env.DEVHUB_URL}/images/:image*.(png|jpg|jpeg|gif)`,
-        regex: '^/images/(.+)\\.(png|jpg|jpeg|gif)$',
     },
 ];
 
