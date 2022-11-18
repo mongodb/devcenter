ce1539989dbfdb9358a80e8199d45f15320d17fb
Maxwell Melo <max.melo@mongodb.com>
2022-11-18 13:35:07 -0500

    readd memo to mobilefilters

--- src/components/search-filters/mobile/filters.tsx
+++ src/components/search-filters/mobile/filters.tsx
@@ -22,108 +22,111 @@ interface MobileFiltersProps extends FiltersProps {
     onSort: (sortByValue: string) => void;
 }
 
-const MobileFilters: React.FunctionComponent<MobileFiltersProps> = ({
-    className,
-    filterItems,
-    filters,
-    onFilter,
-    onSort,
-    sortBy,
-    closeModal,
-}) => {
-    const [tempFilters, setTempFilters] = useState<FilterItem[]>(filters);
+const MobileFilters: React.FunctionComponent<MobileFiltersProps> = memo(
+    ({
+        className,
+        filterItems,
+        filters,
+        onFilter,
+        onSort,
+        sortBy,
+        closeModal,
+    }) => {
+        const [tempFilters, setTempFilters] = useState<FilterItem[]>(filters);
 
-    const onToggle = useCallback(
-        (checked: boolean, filter: FilterItem) => {
-            if (checked) {
-                if (filter.subFilters?.length) {
-                    const subFiltersToAdd = filter.subFilters.filter(
-                        subFilter =>
-                            !tempFilters.find(
-                                ({ name, type }) =>
-                                    name === subFilter.name &&
-                                    type === subFilter.type
-                            )
-                    );
-                    setTempFilters(
-                        tempFilters.concat(filter).concat(subFiltersToAdd)
-                    );
+        const onToggle = useCallback(
+            (checked: boolean, filter: FilterItem) => {
+                if (checked) {
+                    if (filter.subFilters?.length) {
+                        const subFiltersToAdd = filter.subFilters.filter(
+                            subFilter =>
+                                !tempFilters.find(
+                                    ({ name, type }) =>
+                                        name === subFilter.name &&
+                                        type === subFilter.type
+                                )
+                        );
+                        setTempFilters(
+                            tempFilters.concat(filter).concat(subFiltersToAdd)
+                        );
+                    } else {
+                        setTempFilters(tempFilters.concat(filter));
+                    }
                 } else {
-                    setTempFilters(tempFilters.concat(filter));
+                    setTempFilters(
+                        tempFilters.filter(
+                            ({ name, type }) =>
+                                !(name === filter.name && type === filter.type)
+                        )
+                    );
                 }
-            } else {
-                setTempFilters(
-                    tempFilters.filter(
-                        ({ name, type }) =>
-                            !(name === filter.name && type === filter.type)
-                    )
-                );
-            }
-        },
-        [tempFilters]
-    );
+            },
+            [tempFilters]
+        );
 
-    return (
-        <div className={className} sx={filtersModal}>
-            <div sx={{ padding: 'inc50', marginBottom: '120px' }}>
-                <div sx={titleSection}>
-                    <TypographyScale>Filter & Sort</TypographyScale>
-                    <ButtonIcon
-                        iconName={ESystemIconNames.CLOSE}
-                        variant="outline"
-                        keepMobileOutline={true}
-                        iconStrokeWeight="medium"
-                        onClick={() => closeModal()}
-                    />
-                </div>
-                <HorizontalRule spacing="small" />
-                <>
-                    <RadioFilterGroup
-                        title="Sort by"
-                        items={[]}
-                        sortBy={sortBy || defaultSortByType}
-                        setSort={onSort}
-                        filters={tempFilters}
-                        isMobile
-                    />
+        return (
+            <div className={className} sx={filtersModal}>
+                <div sx={{ padding: 'inc50', marginBottom: '120px' }}>
+                    <div sx={titleSection}>
+                        <TypographyScale>Filter & Sort</TypographyScale>
+                        <ButtonIcon
+                            iconName={ESystemIconNames.CLOSE}
+                            variant="outline"
+                            keepMobileOutline={true}
+                            iconStrokeWeight="medium"
+                            onClick={() => closeModal()}
+                        />
+                    </div>
                     <HorizontalRule spacing="small" />
-                </>
-                {filterItems.map(({ key, value }) => (
                     <>
-                        <FilterGroup
-                            title={CONTENT_TYPE_NAME_MAP[key]}
-                            allFilters={value}
-                            activeFilters={tempFilters}
-                            onToggle={onToggle}
-                            mobile
-                            key={key}
+                        <RadioFilterGroup
+                            title="Sort by"
+                            items={[]}
+                            sortBy={sortBy || defaultSortByType}
+                            setSort={onSort}
+                            filters={tempFilters}
+                            isMobile
                         />
                         <HorizontalRule spacing="small" />
                     </>
-                ))}
+                    {filterItems.map(({ key, value }) => (
+                        <>
+                            <FilterGroup
+                                title={CONTENT_TYPE_NAME_MAP[key]}
+                                allFilters={value}
+                                activeFilters={tempFilters}
+                                onToggle={onToggle}
+                                mobile
+                                key={key}
+                            />
+                            <HorizontalRule spacing="small" />
+                        </>
+                    ))}
+                </div>
+                <div sx={buttonSection}>
+                    <Button
+                        variant="secondary"
+                        onClick={() => {
+                            onFilter([]);
+                            setTempFilters([]);
+                        }}
+                    >
+                        Clear
+                    </Button>
+                    <Button
+                        onClick={() => {
+                            onFilter(tempFilters);
+                            closeModal();
+                        }}
+                    >
+                        Apply{' '}
+                        {tempFilters.length ? `(${tempFilters.length})` : ''}
+                    </Button>
+                </div>
             </div>
-            <div sx={buttonSection}>
-                <Button
-                    variant="secondary"
-                    onClick={() => {
-                        onFilter([]);
-                        setTempFilters([]);
-                    }}
-                >
-                    Clear
-                </Button>
-                <Button
-                    onClick={() => {
-                        onFilter(tempFilters);
-                        closeModal();
-                    }}
-                >
-                    Apply {tempFilters.length ? `(${tempFilters.length})` : ''}
-                </Button>
-            </div>
-        </div>
-    );
-};
+        );
+    }
+);
 
 MobileFilters.displayName = 'MobileFilters';
 
