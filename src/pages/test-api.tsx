f9870e49f04600d8a90fffb16247ede00d9d1533
harika.kotipalli <harika.1818@gmail.com>
2022-11-18 14:17:19 -0500

    testing service to service call

--- src/pages/test-api.tsx
+++ src/pages/test-api.tsx
@@ -11,14 +11,18 @@ const Page: NextPage<PageProps> = ({ data }) => {
 // This gets called on every request
 export async function getServerSideProps() {
     //Fetch data from external API
-    const res = await fetch(
-        `https://devcenter-backend.devrel.staging.corp.mongodb.com/api/test`
-    );
-
     let data = "couldn't make call to kanopy";
-    if (res.status >= 200 && res.status <= 299) {
-        data = await res.json();
-    }
+
+    await fetch(`http://devcenter-backend-web-app/api/test`)
+        .then(async res => {
+            console.log(res);
+            data = await res.json();
+        })
+        .catch(err => console.log(err));
+
+    // if (res.status >= 200 && res.status <= 299) {
+    //     data = await res.json();
+    // }
     // Pass data to the page via props
     return { props: { data } };
 }
