590f775523947429a8b09879db1782f5ee0fc399
Matt Colella <matt.colella@mongodb.com>
2022-12-07 15:19:04 -0500

    Use auto height CodeSnippet for smaller code blocks

--- src/components/article-body/body-components/codeblock.tsx
+++ src/components/article-body/body-components/codeblock.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import { CodeSnippet } from '@mdb/flora';
 import { getFloraLanguage } from '../../../utils/get-flora-language';
 import {
@@ -10,6 +11,17 @@ export const CodeBlock = ({ lang, value }: { lang: string; value: string }) => {
     const height = getCodeHeight(numberOfLines);
     const language = getFloraLanguage(lang);
 
+    // This is a workaround to a bug where a long line of code will be cut off at the end.
+    // This seems to be a CodeMirror issue, but is especially apparent here without line wrapping.
+    // Basically this will force the component to rerender after 1 second, which allows it to recalculate the line length.
+    // https://github.com/codemirror/codemirror5/issues/5639 describes a similar issue.
+    const [_, setReady] = useState(false);
+    useEffect(() => {
+        setTimeout(() => {
+            setReady(true);
+        }, 1000);
+    }, []);
+
     // Need this because Safari/mobile browsers don't normally render the border radius on scroll containers without this.
     // https://stackoverflow.com/a/58283449
     const styling = {
@@ -17,7 +29,12 @@ export const CodeBlock = ({ lang, value }: { lang: string; value: string }) => {
     };
     return (
         <div sx={styling}>
-            <CodeSnippet height={height} language={language} code={value} />
+            <CodeSnippet
+                height={height}
+                language={language}
+                code={value}
+                lineWrapping={false}
+            />
         </div>
     );
 };
