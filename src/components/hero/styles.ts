949aa241ba6ea5777ca3a77ba60af1730522db80
Justin McKenzie <justin.mckenzie@mongodb.com>
2022-12-08 18:27:51 -0500

    progress

--- src/components/hero/styles.ts
+++ src/components/hero/styles.ts
@@ -1,14 +1,15 @@
 import theme from '@mdb/flora/theme';
+import { ThemeUICSSObject } from 'theme-ui';
 
 export const heroContainerStyles = {
     bg: 'purple10',
     padding: ['inc40', null, 'inc50', 'inc70'],
 };
 
-export const CTAContainerStyles = {
+export const CTAContainerStyles: ThemeUICSSObject = {
     alignItems: [null, null, 'end', 'center'],
     display: ['none', null, 'flex'],
-    flexDirection: [null, null, 'column' as 'column', 'row' as 'row'],
+    flexDirection: [null, null, 'column', 'row'],
     gap: [null, null, 'inc30', 'inc50'],
     gridColumn: [null, null, 'span 3', 'span 7'],
     justifyContent: [null, null, 'end'],
@@ -16,8 +17,8 @@ export const CTAContainerStyles = {
 };
 
 // This is all to complement Flora's animation on the link arrow and force it not to push itself left.
-export const CTALinkStyles = {
-    position: 'relative' as 'relative',
+export const CTALinkStyles: ThemeUICSSObject = {
+    position: 'relative',
     transitionDuration: theme.motion.linkAnimation,
     transitionProperty: 'right margin-left',
     right: '0',
