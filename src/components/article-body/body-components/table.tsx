7d9166da4077a55efde02f31526910ca00a4c729
Matt Colella <matt.colella@mongodb.com>
2023-01-27 10:31:14 -0500

    Table width 100%, headers left-aligned

--- src/components/article-body/body-components/table.tsx
+++ src/components/article-body/body-components/table.tsx
@@ -4,13 +4,12 @@ import { ThemeUICSSObject } from 'theme-ui';
 import { ComponentFactory } from '../component-factory';
 import { ArticleNode } from '../../../interfaces/article-body-node';
 
-const headerCellStyles = (align: any, index: number): ThemeUICSSObject =>
-    ({
-        py: ['inc20', null, 'inc40'],
-        px: ['inc20', null, 'inc30'],
-        textAlign: align[index],
-        whiteSpace: 'nowrap',
-    } as ThemeUICSSObject);
+const headerCellStyles: ThemeUICSSObject = {
+    py: ['inc20', null, 'inc40'],
+    px: ['inc20', null, 'inc30'],
+    textAlign: 'left',
+    whiteSpace: 'nowrap',
+};
 
 const headerRowStyles: ThemeUICSSObject = {
     borderBottomWidth: 'inc30',
@@ -23,17 +22,11 @@ const getNestedValue = (p: any, o: any) => {
     return p.reduce((xs: any, x: any) => (xs && xs[x] ? xs[x] : null), o);
 };
 
-const TableHeading = ({
-    align,
-    headingRow,
-}: {
-    align: any;
-    headingRow: any;
-}) => (
+const TableHeading = ({ headingRow }: { headingRow: any }) => (
     <thead>
         <tr sx={headerRowStyles}>
             {headingRow.map((column: ArticleNode, colIndex: number) => (
-                <th sx={headerCellStyles(align, colIndex)} key={colIndex}>
+                <th sx={headerCellStyles} key={colIndex}>
                     <ComponentFactory
                         nodeData={getNestedValue(['children', 0], column)}
                     />
@@ -49,8 +42,14 @@ export const Table = ({ align, children }: { align: any; children: any }) => {
         const otherRows = children.slice(1);
         return (
             <div sx={{ overflowX: 'auto' }}>
-                <table sx={{ borderCollapse: 'collapse', border: 'none' }}>
-                    <TableHeading align={align} headingRow={headingRow} />
+                <table
+                    sx={{
+                        borderCollapse: 'collapse',
+                        border: 'none',
+                        width: '100%',
+                    }}
+                >
+                    <TableHeading headingRow={headingRow} />
                     {otherRows.map((data: ArticleNode, i: number) => (
                         // Pass the align array so we can apply alignment in TableCell
                         <ComponentFactory
