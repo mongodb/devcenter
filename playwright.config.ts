40cd60adafc1555291e63b2ab877918473f9cb6f
Maxwell Melo <max.melo@mongodb.com>
2023-01-27 13:31:07 -0500

    add mock name in events playwright test, update events mock

--- playwright.config.ts
+++ playwright.config.ts
@@ -6,13 +6,13 @@ const config: PlaywrightTestConfig = {
     webServer: {
         command: 'yarn start',
         port: 3000,
-        timeout: 120 * 1000,
+        timeout: 480 * 1000,
         reuseExistingServer: !process.env.CI,
     },
     use: {
         screenshot: 'only-on-failure',
     },
     outputDir: './playwright-artifacts',
-    timeout: 120 * 1000,
+    timeout: 480 * 1000,
 };
 export default config;
