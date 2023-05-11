6d7d752d5d93fbfd6662035573c6376cc907a44b
harika.kotipalli <harika.1818@gmail.com>
2023-05-11 15:18:56 -0400

    Pointing featured content to content stack

--- src/api-requests/get-featured-for-topic.ts
+++ src/api-requests/get-featured-for-topic.ts
@@ -1,6 +1,11 @@
 import { UnderlyingClient } from '../types/client-factory';
 import { ApolloQueryResult, gql } from '@apollo/client';
-import { FeaturedResponse } from '../interfaces/featured';
+import {
+    CS_FeaturedContentResponse,
+    FeaturedResponse,
+} from '../interfaces/featured';
+import { CS_HEADERS } from '../data/constants';
+import axios from 'axios';
 
 export const getFeaturedForTopicFromAPI = async (
     client: UnderlyingClient<'ApolloREST'>,
@@ -26,8 +31,104 @@ export const getFeaturedForTopicFromAPI = async (
             }
         }
     `;
+
     const { data }: ApolloQueryResult<{ featured: FeaturedResponse }> =
         await client.query({ query });
+    console.log(data);
 
     return data.featured;
+};
+
+const CS_featuredContentFields = `
+      categoryConnection {
+        edges {
+          node {
+            ... on L1Products {
+              calculated_slug
+            }
+            ... on ContentTypes {
+              calculated_slug
+            }
+            ... on ProgrammingLanguages {
+              calculated_slug
+            }
+            ... on Technologies {
+              calculated_slug
+            }
+          }
+        }
+      }
+      industry_eventsConnection(limit: 3) {
+        edges {
+          node {
+            ... on IndustryEvents {
+              title
+            }
+          }
+        }
+      }
+      podcastsConnection(limit: 3) {
+        edges {
+          node {
+            ... on Podcasts {
+              title
+            }
+          }
+        }
+      }
+      videosConnection(limit: 3) {
+        edges {
+          node {
+            ... on Videos {
+              title
+            }
+          }
+        }
+      }
+      articlesConnection(limit: 3) {
+        edges {
+          node {
+            ... on Articles {
+              title
+            }
+          }
+        }
+      }
+`;
+
+export const getAllFeaturedContent = () => `
+    query get_featured {
+        all_featured_content  {
+            items {
+                ${CS_featuredContentFields}
+            }
+        }
+    }
+`;
+
+export const getFeaturedForTopic = (topicSlug: string) => `        
+    query get_featured {
+        all_featured_content(
+            where: {category: {MATCH: ANY, 
+            content_types: {calculated_slug: "${topicSlug}"}, 
+            l1_products: {calculated_slug: "${topicSlug}"}, 
+            programming_languages: {calculated_slug: "${topicSlug}"}, 
+            technologies: {calculated_slug: "${topicSlug}"}}}
+        ) {
+            items {
+                ${CS_featuredContentFields}
+            }
+        }
+    }
+`;
+
+export const CS_getAllFeaturedContent = async (): Promise<
+    CS_FeaturedContentResponse[]
+> => {
+    const url = `${
+        process.env.CS_GRAPHQL_URL
+    }?environment=production&query=${getAllFeaturedContent()}`;
+    const { data } = await axios.get(url, { headers: CS_HEADERS });
+    const { items } = data.data.all_featured_content;
+    return items;
 };
