30b6c328d8a21d7f075746f4413edb0c4eed8c11
harika.kotipalli <harika.1818@gmail.com>
2023-05-11 21:18:50 -0400

    pointing featured to contentstack

--- src/api-requests/get-featured-for-topic.ts
+++ src/api-requests/get-featured-for-topic.ts
@@ -1,112 +1,12 @@
-import { UnderlyingClient } from '../types/client-factory';
-import { ApolloQueryResult, gql } from '@apollo/client';
-import {
-    CS_FeaturedContentResponse,
-    FeaturedResponse,
-} from '../interfaces/featured';
+import { FeaturedResponse } from '../interfaces/featured';
 import { CS_HEADERS } from '../data/constants';
 import axios from 'axios';
+import {
+    CS_featuredContentFields,
+    transformCSFeaturedContentResponse,
+} from './get-all-featured';
 
-export const getFeaturedForTopicFromAPI = async (
-    client: UnderlyingClient<'ApolloREST'>,
-    topicSlug: string
-): Promise<FeaturedResponse> => {
-    const query = gql`
-        query Featured {
-            featured @rest(type: "Featured", path: "/featured-content/${encodeURIComponent(
-                topicSlug
-            )}") {
-                articles: new_articles {
-                    title: name
-                }
-                podcasts: podcasts {
-                    title
-                }
-                videos: new_videos {
-                    title
-                }
-                events: events {
-                    title
-                }
-            }
-        }
-    `;
-
-    const { data }: ApolloQueryResult<{ featured: FeaturedResponse }> =
-        await client.query({ query });
-    console.log(data);
-
-    return data.featured;
-};
-
-const CS_featuredContentFields = `
-      categoryConnection {
-        edges {
-          node {
-            ... on L1Products {
-              calculated_slug
-            }
-            ... on ContentTypes {
-              calculated_slug
-            }
-            ... on ProgrammingLanguages {
-              calculated_slug
-            }
-            ... on Technologies {
-              calculated_slug
-            }
-          }
-        }
-      }
-      industry_eventsConnection(limit: 3) {
-        edges {
-          node {
-            ... on IndustryEvents {
-              title
-            }
-          }
-        }
-      }
-      podcastsConnection(limit: 3) {
-        edges {
-          node {
-            ... on Podcasts {
-              title
-            }
-          }
-        }
-      }
-      videosConnection(limit: 3) {
-        edges {
-          node {
-            ... on Videos {
-              title
-            }
-          }
-        }
-      }
-      articlesConnection(limit: 3) {
-        edges {
-          node {
-            ... on Articles {
-              title
-            }
-          }
-        }
-      }
-`;
-
-export const getAllFeaturedContent = () => `
-    query get_featured {
-        all_featured_content  {
-            items {
-                ${CS_featuredContentFields}
-            }
-        }
-    }
-`;
-
-export const getFeaturedForTopic = (topicSlug: string) => `        
+export const getFeaturedForTopicQuery = (topicSlug: string) => `        
     query get_featured {
         all_featured_content(
             where: {category: {MATCH: ANY, 
@@ -122,38 +22,12 @@ export const getFeaturedForTopic = (topicSlug: string) => `
     }
 `;
 
-const transformCSFeaturedContentResponse = (
-    items: CS_FeaturedContentResponse[]
-) => {
-    return items.map(item => {
-        return {
-            articles: item.articlesConnection.edges.map(a => a.node.title),
-            videos: item.videosConnection.edges.map(v => v.node.title),
-            podcasts: item.podcastsConnection.edges.map(p => p.node.title),
-            industry_events: item.industry_eventsConnection.edges.map(
-                i => i.node.title
-            ),
-        } as FeaturedResponse;
-    });
-};
-
-export const CS_getAllFeaturedContent = async (): Promise<
-    FeaturedResponse[]
-> => {
-    const url = `${
-        process.env.CS_GRAPHQL_URL
-    }?environment=production&query=${getAllFeaturedContent()}`;
-    const { data } = await axios.get(url, { headers: CS_HEADERS });
-    const { items } = data.data.all_featured_content;
-    return transformCSFeaturedContentResponse(items);
-};
-
 export const CS_getFeaturedContentForTopic = async (
-    topicSlug
+    topicSlug: string
 ): Promise<FeaturedResponse> => {
     const url = `${
         process.env.CS_GRAPHQL_URL
-    }?environment=production&query=${getFeaturedForTopic(topicSlug)}`;
+    }?environment=production&query=${getFeaturedForTopicQuery(topicSlug)}`;
     const { data } = await axios.get(url, { headers: CS_HEADERS });
     const { items } = data.data.all_featured_content;
     return transformCSFeaturedContentResponse(items)[0];
