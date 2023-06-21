560c461a4cec62af0bc6c4a75f3a2153d9f86485
harika.kotipalli <harika.1818@gmail.com>
2023-06-21 00:41:47 -0400

    Contentstack preview

--- src/utils/stack.js
+++ src/utils/stack.js
@@ -1,6 +1,6 @@
-import * as Contentstack from 'contentstack';
+import { Stack } from 'contentstack';
 
-export const stack = Contentstack.Stack({
+export const stackConfig = {
     api_key: process.env.api_key,
     delivery_token: process.env.delivery_token,
     environment: process.env.environment,
@@ -9,8 +9,8 @@ export const stack = Contentstack.Stack({
         enable: true,
         host: 'api.contentstack.io',
     },
-});
+};
 
 export const initializeContentStackSdk = () => {
-    return stack;
+    return Stack(stackConfig);
 };
