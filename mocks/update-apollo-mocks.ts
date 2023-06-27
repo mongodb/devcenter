b8b4b1bdd318b9966b17efdff24f973618bbaebe
Jacky Wong <jacky.wong@mongodb.com>
2023-06-27 13:22:54 -0400

    Add logic to asterisk mock

--- mocks/update-apollo-mocks.ts
+++ mocks/update-apollo-mocks.ts
@@ -120,6 +120,28 @@ export const queryInfos: QueryInfo[] = [
     },
 ];
 
+// currently not applied due to
+// frontend is not consistently handling asterisked titles
+// const asteriskTitlesForDistinction = (mockData: any) => {
+//     if (!mockData) return mockData;
+
+//     if (Array.isArray(mockData)) {
+//         mockData = mockData.map(md => asteriskTitlesForDistinction(md));
+//     }
+
+//     if (typeof mockData === 'object' && !Array.isArray(mockData)) {
+//         if (mockData.hasOwnProperty('title')) {
+//             mockData.title = `*${mockData.title}`;
+//         }
+
+//         Object.keys(mockData).forEach(key => {
+//             mockData[key] = asteriskTitlesForDistinction(mockData[key]);
+//         });
+//     }
+
+//     return mockData;
+// };
+
 const updateLocalMockData = async () => {
     for (const { query, contentTypeUID, variables } of queryInfos) {
         const mockData = await fetchAllForMocks(
@@ -127,6 +149,14 @@ const updateLocalMockData = async () => {
             contentTypeUID,
             variables
         );
+
+        // below is commented out because
+        // frontend is not consistently handling asterisked titles
+        // so data without asterisks is used for mocking
+
+        // const mockClone = JSON.parse(JSON.stringify(mockData));
+        // const modifiedMock = asteriskTitlesForDistinction(mockClone);
+        // const jsonData = JSON.stringify(modifiedMock, null, 2);
         const jsonData = JSON.stringify(mockData, null, 2);
         const fileName = `./${contentTypeUID}.json`;
         const filePath = path.join(__dirname, 'apollo-data', fileName);
