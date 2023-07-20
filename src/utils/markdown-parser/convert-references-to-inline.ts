6ba6e4a7d47262c69d9f249ed994da154b8bfcd4
harika.kotipalli <harika.1818@gmail.com>
2023-07-20 10:52:06 -0400

    update remark parser

--- src/utils/markdown-parser/convert-references-to-inline.ts
+++ src/utils/markdown-parser/convert-references-to-inline.ts
@@ -1,6 +1,10 @@
+import remarkInlineLinks from 'remark-inline-links';
+import { remark } from 'remark';
+
 export const convertReferencesToInline = async (content: string) => {
     const markdownWithReferences = content || '';
-    //const markdownWithInLine = await remark().use(remarkInlineLinks).process(markdownWithReferences)
-    //return markdownWithInLine.toString()
-    return markdownWithReferences;
+    const markdownWithInLine = await remark()
+        .use(remarkInlineLinks)
+        .process(markdownWithReferences);
+    return markdownWithInLine.toString();
 };
