dcc74ec228f4c2e5f59f3bd54b47b3bbf400f472
harika.kotipalli <harika.1818@gmail.com>
2023-08-10 22:36:48 -0400

    testing production build

--- playwright-tests/pages/code-examples.spec.ts
+++ playwright-tests/pages/code-examples.spec.ts
@@ -1,6 +1,7 @@
 import { test } from '@playwright/test';
 import { runPercy } from '../utils';
 
+//test
 test(
     'Code Examples visual regression testing',
     runPercy('/developer/code-examples', 'Code Examples')
