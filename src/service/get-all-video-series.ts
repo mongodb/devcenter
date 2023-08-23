660e745d13f37fb9c2117131bab7653c361efee1
harika.kotipalli <harika.1818@gmail.com>
2023-08-23 14:50:40 -0400

    DEVHUB-1813 removing all the references to strapi environmental variables

--- src/service/get-all-video-series.ts
+++ src/service/get-all-video-series.ts
@@ -1,8 +1,8 @@
 import { CS_SeriesResponse, Series } from '../interfaces/series';
-import { CS_parseStrapiSeriesResponse } from './parse-strapi-series-response';
+import { CS_parseSeriesResponse } from './parse-series-response';
 import { CS_getAllVideoSeriesFromAPI } from '../api-requests/get-video-series';
 
 export const getAllVideoSeries = async (): Promise<Series[]> => {
     const data: CS_SeriesResponse[] = await CS_getAllVideoSeriesFromAPI();
-    return CS_parseStrapiSeriesResponse(data);
+    return CS_parseSeriesResponse(data);
 };
