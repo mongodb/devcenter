85e60d54545ceb4c95ba40bbaba724999f8083e6
harika.kotipalli <harika.1818@gmail.com>
2023-08-23 22:43:47 -0400

    Adding remark-inline-links to convert referenced links and images to inline

---
+++ src/utils/markdown-parser/convert-references.ts
@@ -0,0 +1,10 @@
+import remarkInlineLinks from 'remark-inline-links';
+import { remark } from 'remark';
+
+export const convertReferences = async (content: string) => {
+    const markdownWithReferences = content || '';
+    const markdownWithInLine = await remark()
+        .use(remarkInlineLinks)
+        .process(markdownWithReferences);
+    return markdownWithInLine.toString();
+};
