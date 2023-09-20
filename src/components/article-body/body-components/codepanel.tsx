20baf874b8bf36dc3ef4e49654b72474ace5b83e
harika.kotipalli <harika.1818@gmail.com>
2023-09-20 13:11:33 -0400

    DEVHUB-1186 setting the codeblock height to auto for all codeblocks, adding a horizontal bar

--- src/components/article-body/body-components/codepanel.tsx
+++ src/components/article-body/body-components/codepanel.tsx
@@ -1,9 +1,5 @@
 import { CodePanel } from '@mdb/flora';
 import { getFloraLanguage } from '../../../utils/get-flora-language';
-import {
-    getCodeHeight,
-    getNumberOfLines,
-} from '../../../utils/get-code-height';
 import { LanguageModeType } from '@mdb/flora/dist/types/src/CodeSnippet/types';
 
 type Snippet = {
@@ -24,14 +20,11 @@ type Tab = {
 
 export const CodeTabs = ({ children }: any) => {
     const snippets: Snippet[] = [];
-    let maxNumberOfLines = 0;
     children.map((tabs: Tabs) => {
         const { tabid } = tabs.options;
         tabs.children.map((tab: Tab) => {
             const { type, value } = tab;
             if (type === 'code') {
-                const numberOfLines = getNumberOfLines(value);
-                maxNumberOfLines = Math.max(numberOfLines, maxNumberOfLines);
                 snippets.push({
                     language: getFloraLanguage(tabid.toLowerCase()),
                     code: value,
@@ -42,11 +35,7 @@ export const CodeTabs = ({ children }: any) => {
     });
     return (
         snippets.length > 0 && (
-            <CodePanel
-                snippets={snippets}
-                height={getCodeHeight(maxNumberOfLines)}
-                lineWrapping={false}
-            />
+            <CodePanel snippets={snippets} height="auto" lineWrapping={false} />
         )
     );
 };
