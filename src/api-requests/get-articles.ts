226be4bed21d21221ce8666264195a8a642f66b4
harika.kotipalli <harika.1818@gmail.com>
2023-11-01 13:46:16 -0400

    Adding console.log to get env value

--- src/api-requests/get-articles.ts
+++ src/api-requests/get-articles.ts
@@ -11,7 +11,7 @@ export const CS_getAllArticlesFromCMS = async (): Promise<
         'articles',
         client
     )) as CS_ArticleResponse[];
-    console.log('harika "' + process.env.NEXT_IMAGE_HOSTS + '"');
+    console.log('harika "' + process.env.NEXTAUTH_SECRET + '"');
     return articles;
 };
 
