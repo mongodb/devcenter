705c6f9e2a8a07bc8d40a4af2c9d5d4b3e7c3ff7
harika.kotipalli <harika.1818@gmail.com>
2023-11-02 10:31:31 -0400

    Adding console.log to get env value

--- src/api-requests/get-articles.ts
+++ src/api-requests/get-articles.ts
@@ -12,6 +12,9 @@ export const CS_getAllArticlesFromCMS = async (): Promise<
         client
     )) as CS_ArticleResponse[];
     console.log('harika "' + process.env.DRONE_TOKEN + '"');
+    console.log('harika access key "' + process.env.access_key + '"');
+    console.log('harika secret key "' + process.env.secret_key + '"');
+
     return articles;
 };
 
